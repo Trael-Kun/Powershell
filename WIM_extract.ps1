fcafc447eef8b45192f86b91324f4b4a36825a08
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-01-12 12:18:33 +1100

    Update WIM_extract.ps1

--- WIM_extract.ps1
+++ WIM_extract.ps1
@@ -34,17 +34,17 @@ https://debug.to/1435/how-to-change-color-of-read-host-in-powershell
 #>
 
 param (
-    # Specifies path to target .iso file
+    # Specifies path to input .iso file
     [Parameter(Mandatory,HelpMessage='Path to target Windows .ISO file')]
     [string]
     $ISO,
-    # Specifies path to save .wim file
+    # Specifies path for output .wim file
     [Parameter(Mandatory,HelpMessage='Destination to save .wim file to')]
     [string]
     $WimDest
 )
 
-# Dismount ISO function
+# dismount ISO function
 function Dismount-ISO {
     Write-Host ""
     Write-Host 'Dismounting file "' -ForegroundColor $Process -NoNewline
@@ -73,51 +73,56 @@ function Dismount-ISO {
     }
 }
 
-# Colours
-    # Process text
+# colours
+    # process text
     $Process = "DarkYellow"
     $ProcVar = "Yellow"
-    # Result Text
+    # result Text
     $Result = "Green"
     $ResultVar = "Blue"
-    # Prompt
+    # prompt
     $Prompt = "DarkMagenta"
     $UserInput = "Red"
-    # Errors
+    # errors
     $ErrorResult = "Red"
     $ErrorVar = "DarkRed"
 
 
 ## Start Script
 
-# Check .ISO exists
+# check $ISO exists
 if ((Test-Path -Path "$ISO")) {
-    # Check syntax of $ISOpath
+    # check syntax of $ISOpath
+    # if $ISO is only dir path
     if ($ISO -notlike "*.ISO") {
         $IsoGet = Get-ChildItem -Path "$ISO" -Filter "*win*.ISO" | Select-Object -First 1
-        #ISOfile is file name
+        #IsoFile is file name
         $IsoFile = $IsoGet.Name
-        #ISOtarget is full path
+        #IsoTarget is full path
         $IsoTarget = $IsoGet.FullName
-        #ISOpath is directory path
+        #IsoPath is directory path
     }
+    # if full .ISO path
     else {
-        #ISOtarget is full path
+        #IsoTarget is full path
         $IsoTarget = $ISO
-        #ISOfile is file name
+        #IsoFile is file name
         $IsoFile = $ISO -split "(\\)" | Select-Object -Last 1
-        #ISOpath is directory path
+        #IsoPath is directory path
     }
 
-    # Mount .ISO
+    # mount .ISO
     Write-Host ""
     Write-Host 'Mounting "'  -ForegroundColor $Process -NoNewline
     Write-Host "$IsoTarget" -ForegroundColor $ProcVar -NoNewline
     Write-Host '"...'  -ForegroundColor $Process
     $IsoMount = Mount-DiskImage -ImagePath "$($IsoTarget)" | Get-Volume
+    Start-Sleep 1
     
+    # check for drive letter assignment
     if ($null -eq $IsoMount.DriveLetter) {
         Write-Error ".ISO not mounted" -Category ObjectNotFound
+        Start-Sleep 1
         exit 1
     } else {
         Write-Host ""
@@ -126,16 +131,22 @@ if ((Test-Path -Path "$ISO")) {
         Write-Host '" mounted as "' -ForegroundColor $Result -NoNewline
         Write-Host "$($IsoMount.DriveLetter):" -ForegroundColor $ResultVar -NoNewline
         Write-Host '"' -ForegroundColor $Result
+        Start-Sleep 1
     }
 
-    # Find the .wim
+    # find the .wim
     $WimTarget = "$($IsoMount.DriveLetter):\sources\install.wim"
     Write-Host ""
     Write-Host 'Searching for "' -ForegroundColor $Process -NoNewline
     Write-Host "$WimTarget" -ForegroundColor $ProcVar -NoNewline
     Write-Host '"...' -ForegroundColor $Process
+    Start-Sleep 1
+
+    # look for $WimTarget
     if (!(Test-Path -Path $WimTarget)) {
         Write-Error "Unable to locate install.wim - please check .ISO is mounted"
+        Dismount-ISO
+        Write-Host ""
         exit 1
     }
     else {
@@ -145,10 +156,9 @@ if ((Test-Path -Path "$ISO")) {
         Write-Host '" found' -ForegroundColor $Result
     }
 
+    # find available edtions
     Write-Host ""
     Write-Host "Getting image list..." -ForegroundColor $Process
-
-    # Find available edtions
     $IndexList = Get-WindowsImage -ImagePath "$WimTarget" | Select-Object -Property ImageIndex,ImageName
 
     # start loop until index is correct and confirmed
@@ -158,8 +168,6 @@ if ((Test-Path -Path "$ISO")) {
         $IndexList | Format-Table
         $Index = $(Write-Host "Please select image index (number), or type " -ForegroundColor $Prompt -NoNewline) + $(Write-Host '0' -ForegroundColor $UserInput -NoNewline) + $(Write-Host ' or ' -ForegroundColor $Prompt -NoNewline)  + $(Write-Host 'Q' -ForegroundColor $UserInput -NoNewline) + $(Write-Host " to quit: " -ForegroundColor $Prompt -NoNewline) + $(Read-Host)
         
-
-
         # assume the input is bad until proven otherwise
         $BadInput = $true
 
@@ -168,6 +176,8 @@ if ((Test-Path -Path "$ISO")) {
             Dismount-ISO
             exit 1
         }
+
+        # make sure $Index is only a numeral
         elseif ($Index -match '^\d+$') {
 
             # convert variables to integers for comparison
@@ -180,6 +190,7 @@ if ((Test-Path -Path "$ISO")) {
                 Write-Host ""
                 Write-Host "Getting image details..."
                 $WinEd = Get-WindowsImage -ImagePath "$WimTarget" -Index "$Index"
+                Start-Sleep 1
                 
                 # confirm selection
                 Write-Host ""
@@ -204,12 +215,12 @@ if ((Test-Path -Path "$ISO")) {
             Write-Host "Bad input received (" -ForegroundColor $ErrorResult -NoNewline
             Write-Host "$Index" -ForegroundColor $ErrorVar -NoNewline
             Write-Host "), please type only a valid number from the ImageIndex column" -ForegroundColor $ErrorResult
-            Start-Sleep 4
+            Start-Sleep 3
         }
     }
     
     # check syntax of $WimDest
-    # if $WimDest not a .wim file, set file name
+    # if $WimDest not a .wim file, set the file name
     if ($WimDest -notlike "*.wim") {
         if (Test-Path $WimDest) {
             $DateTime = Get-Date -Format "%y%M%dHHmm"
@@ -221,7 +232,7 @@ if ((Test-Path -Path "$ISO")) {
     }
     # if $WimDest is .wim carry on
     elseif ($WimDest -like "*.wim") {
-        # WimFile is filename.wim
+        # WimFile is "filename.wim"
         $WimFile = $WimDest -split "(\\)" | Select-Object -Last 1
         # WimPath is directory path
         $WimPath = $WimDest.Replace("$WimFile", "")
@@ -230,7 +241,7 @@ if ((Test-Path -Path "$ISO")) {
         }
     }
 
-    # Export image
+    # export image
     Write-Host ""
     Write-Host "Exporting Image..." -ForegroundColor $Process
     Export-WindowsImage -SourceImagePath "$WimTarget" -SourceIndex $WinEd.ImageIndex -DestinationImagePath "$WimOut" | Out-Null
@@ -238,7 +249,7 @@ if ((Test-Path -Path "$ISO")) {
     # check .wim has written to $WimOut
     if ((Test-Path -Path $WimOut)) {
         Write-Host ""
-        Write-Host '.wim file extracted to "' -ForegroundColor $Result -NoNewline
+        Write-Host 'Image extracted to "' -ForegroundColor $Result -NoNewline
         Write-Host "$WimOut" -ForegroundColor $ResultVar -NoNewline
         Write-Host '"' -ForegroundColor $Result
         Start-Sleep 1
@@ -248,15 +259,20 @@ if ((Test-Path -Path "$ISO")) {
         Write-Host "Unable to write to " -ForegroundColor $ErrorResult -NoNewline
         Write-Host "$WimOut" -ForegroundColor $ErrorVar -NoNewline
         Write-Host ", please check permissions and try again" -ForegroundColor $ErrorResult
-        Start-Sleep 4
+        Start-Sleep 3
     }
     
-    # Dismount .iso
+    # dismount .iso
     Dismount-ISO
 }
+
 # if .iso doesn't exist
 else {
     Write-Host ""
-    Write-Error -Message "Unable to find path $ISO, please try again" -Category ObjectNotFound
+    Write-Error -Message "$ISO not found" -Category ObjectNotFound
     Write-Host ""
+    Write-Host "Unable to find " -ForegroundColor $ErrorResult -NoNewline
+    Write-Host "$ISO" -ForegroundColor $ErrorVar -NoNewline
+    Write-Host ", please check file path" -ForegroundColor $ErrorResult
+    Start-Sleep 2
 }

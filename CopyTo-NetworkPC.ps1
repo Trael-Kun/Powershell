7e7d8e2cf4198b305c6c74b2657eeb2b5650a08f
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-02-05 09:55:32 +1100

    Update CopyTo-NetworkPC.ps1

--- CopyTo-NetworkPC.ps1
+++ CopyTo-NetworkPC.ps1
@@ -1,38 +1,103 @@
 <#
-copy a file from local folder to remote PC with different credentials
+.SYNOPSIS
+    Copy a file to multiple PCs from Jumpbox
 
-Bill
-02/01/24
+.DESCRIPTION
+    Copies a single file from a location accessible from the local PC and copies it to a location on a remote 
+    PC using different credentials.
+    The location on the remote PC is mounted as a network drive during copying, and is then unmounted so that 
+    the drive letter can be reused for the next loop.
 
-https://stackoverflow.com/questions/612015/copy-item-with-alternate-credentials
-https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/remove-psdrive?view=powershell-7.4
+.NOTES
+    Author:     Bill Wilson https://github.com/Trael-Kun
+    Created:    02/01/24
+
+    References: 
+    https://stackoverflow.com/questions/612015/copy-item-with-alternate-credentials
+    https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/remove-psdrive?view=powershell-7.4
+    https://lazyadmin.nl/powershell/new-psdrive/
+    https://stackoverflow.com/questions/38442196/powershell-how-to-user-input-array-cycling-through-it-and-then-closing-out-at
+    https://stackoverflow.com/questions/40009297/net-use-in-powershell-without-specifying-drive
 #>
 
-#Computers
-$PCs = @(
-    "W001",
-    "W002",
-    "W042",
-    "W104",
-    "L246"
-)
-#Credentials
-$Cred = Get-Credential
-#Folder where your file/s at
-$SourceDir = "C:\Temp"
-#File name, use "" for full directory
-$SourceFile = "Words.txt"
-#Drive letter
-$DriveLetter = "J"
+# Header
+Write-Host "Copy a single file to multiple computers" -ForegroundColor Green
+Write-Host ""
 
-foreach ($PC in $PCs) {
-    $Dest   = "\\$PC\c$\Temp"
-    New-PSDrive -Name $Drive -PSProvider FileSystem -Root $Dest -Credential $cred -Persist
-    Copy-Item -Path $Source\$SourceFile -Destination "$DriveLetter:\$SourceFile"
-        if (Test-Path "$DriveLetter:\$SourceFile") {
-            Write-Host "Copied to $PC"
+# PC Numbers
+# Keep prompting for another until they just press enter
+$PCs = do  { 
+    $PC = Read-Host "Enter remote computer, or blank to finish" -ForegroundColor Yellow
+    $PC
+} while ($PC -ne '')
+Write-Host ""
+
+#Get Credentials
+Write-Host "Enter credentials to access remote PCs" -ForegroundColor Yellow
+Write-Host ""
+$Cred = Get-Credential
+
+#Folder where your file/s at
+$SourceDir = Read-Host "Source directory path" -ForegroundColor Yellow
+Write-Host ""
+
+#File name, use "" for full directory
+$SourceFile = Read-Host "Source Filename" -ForegroundColor Yellow
+Write-Host ""
+#Join paths
+$SourcePath = Join-Path -Path $SourceDir -ChildPath $SourceFile
+
+#Hard-coded drive letter as storing as a variable is causing issues
+$DestDir = Read-Host "Destination path on remote PC (e.g. C:\Temp)" -ForegroundColor Yellow
+Write-Host ""
+
+#Get the drive letter
+$DestPath = $DestDir.Replace(':','$')
+
+# Now do the thing
+foreach ($Asset in ($PCs | Select-Object -SkipLast 1)) {
+    if ($Asset.length -ne '0') {
+        if ($Asset -match '^VM') {
+            $PCname = "$Asset"
         }
-        else { 
-            write-host "$PC copy failed" }
-   Get-PSDrive $DriveLetter |  Remove-PSDrive
+        elseif ($Asset -match '^LAP') {
+            $PCname = "$Asset"
+        }
+        elseif ($Asset -match '^CAF') {
+            $PCname = "$Asset"
+        }
+        elseif ($Asset -match '^WKS') {
+            $PCname = "$Asset"
+        }
+        # if it's none of the above, function will assume it's WKS
+        elseif ($Asset -notmatch '^WKS') {
+            $PCname = "WKS$Asset"
+        }
+    #Set remote PC as network drive
+    $Dest = "\\$PCname\$DestPath"
+    Write-Host "Destination is $Dest" -ForegroundColor Green
+    $Destination = Join-Path -Path $Dest -ChildPath $SourceFile
+    #New-SmbMapping method
+    Write-Host "Mapping $dest as network drive"
+    New-SmbMapping -RemotePath $Dest -UserName $cred.UserName -Password $cred.GetNetworkCredential().password
+        if (Test-Path "$Dest") {
+            Write-Host "$Dest mounted" -ForegroundColor Green
+            Write-Host "Copying $SourcePath to $Destination"
+            Copy-Item -Path $SourcePath -Destination "$Destination" -Force
+            Start-Sleep 2
+            if (Test-Path "$Destination") {
+                Write-Host "Copied to $Destination" -ForegroundColor Green
+            }
+            else {
+                Write-error "$Destination failed to write"
+            }
+        }
+        else {
+            Write-error "$Dest failed to mount"
+        }
+        #unmount the network drive, get ready for the next one
+        Write-Host "Removing mapped drive" -ForegroundColor Yellow
+        Start-Sleep 2
+        Remove-SmbMapping -RemotePath $Dest -Force
+    }
 }

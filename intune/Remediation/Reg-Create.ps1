a7d851c531136d260cce6b18f8f6658677f1f828
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-03-05 15:56:44 +1100

    Update and rename Reg-Create.ps1 to RegkEY-CREATE.ps1

--- intune/Remediation/Reg-Create.ps1
+++
@@ -1,2 +0,0 @@
-New-Item -Path "HKCU:\Software\Micro Focus\Content Manager" -Name OfficeAddins -Force
-New-ItemProperty -Path "HKCU:\Software\Micro Focus\Content Manager\OfficeAddins" -Name UseNativeUI -PropertyType DWord -Value 1

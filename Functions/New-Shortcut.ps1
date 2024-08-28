62679115d2a7432cad93afd8d7db51aae7bab4a9
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-08-28 15:52:57 +1000

    Update New-Shortcut.ps1
    added comments
    added if statements to ensure function functions functionally

--- Functions/New-Shortcut.ps1
+++ Functions/New-Shortcut.ps1
@@ -1,11 +1,19 @@
 function New-Shortcut {
     param (
-        [Parameter(Mandatory=$true)]
-        [string]$Target,
-        [string]$File
+        [string] $LnkPath,                                              # Folder for new shortcut
+        [string] $LnkFile,                                              # Filename for new shortcut, ending in .lnk
+        [string] $LnkTarget,                                            # What the shortcut takes you to
+        [string] $LnkIcon                                               # Icon for the shortcut, if different to the default
     )
-    $WshShell = New-Object -ComObject WScript.Shell
-    $Lnk = $WshShell.CreateShortcut("$File")
-    $Lnk.TargetPath = $Target
-    $Lnk.Save()
+    if ($LnkFile -notmatch '.lnk$') {                                   # check FileName for .lnk filetype
+        $LnkFile +='.lnk'                                               # if not, add it
+    }
+    $Path               = Join-Path -Path $LnkPath -ChildPath $LnkFile  # join path & filename for shortcut
+    $WshShell           = New-Object -ComObject WScript.Shell           # create shortcut object
+    $Lnk                = $WshShell.CreateShortcut("$Path")             # set .lnk path
+    $Lnk.TargetPath     = $LnkTarget                                    # set target
+    if ($null -ne $LnkIcon) {                                           # if LnkIcon has a value
+        $Lnk.IconLocation   = $LnkIcon                                  # set icon
+    }
+    $Lnk.Save()                                                         # create shortcut
 }

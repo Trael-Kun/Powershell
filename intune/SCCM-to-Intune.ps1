3b01cee389e84fbc61288b00ad6bbf11e3469582
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-10-23 16:44:40 +1100

    Update SCCM-to-Intune.ps1
    tidied formatting

--- intune/SCCM-to-Intune.ps1
+++ intune/SCCM-to-Intune.ps1
@@ -7,7 +7,6 @@ param(
 	[Parameter(Mandatory=$False)] [String] $OutputFile = "\\Server01\Autopilot\Autopilot.csv", 
 	[Parameter(Mandatory=$False)] [String] $GroupTag = "AutoPilot"
 )
-
 Begin
 {
 	# Initialize empty list
@@ -18,14 +17,11 @@ Process
 	foreach ($comp in $Name)
 	{
 		$bad = $false
-
 		# Get a CIM session
 		$session = New-CimSession
-
 		# Get the common properties.
 		Write-Verbose "Checking $comp"
 		$serial = (Get-CimInstance -CimSession $session -Class Win32_BIOS).SerialNumber
-
 		# Get the hash (if available)
 		$devDetail = (Get-CimInstance -CimSession $session -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
 		if ($devDetail -and (-not $Force))
@@ -37,27 +33,20 @@ Process
 			$bad = $true
 			$hash = ""
 		}
-
-
 			$make = ""
 			$model = ""
-
 		# Getting the PKID is generally problematic for anyone other than OEMs, so let's skip it here
 		$product = ""
-
-
 			# Create a pipeline object
 			$c = New-Object psobject -Property @{
 				"Device Serial Number" = $serial
 				"Windows Product ID" = $product
 				"Hardware Hash" = $hash
-			}
-			
+			}			
 			if ($GroupTag -ne "")
 			{
 				Add-Member -InputObject $c -NotePropertyName "Group Tag" -NotePropertyValue $GroupTag
 			}
-
 		# Write the object to the pipeline or array
 		if ($bad)
 		{
@@ -73,18 +62,14 @@ Process
 			$computers += $c
 			Write-Host "Gathered details for device with serial number: $serial"
 		}
-
 		Remove-CimSession $session
 	}
 }
-
-
 End
 {
 	if (Test-Path $OutputFile)
 		{
 		    $computers += Import-CSV -Path $OutputFile
 		}
-            $computers | Select-Object "Device Serial Number", "Windows Product ID", "Hardware Hash", "Group Tag" | ConvertTo-CSV -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File $OutputFile
-    
+            $computers | Select-Object "Device Serial Number", "Windows Product ID", "Hardware Hash", "Group Tag" | ConvertTo-CSV -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File $OutputFile   
 }

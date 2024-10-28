3b7cb990f8026d8574e02681dea5a10672ebdf8d
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-10-28 15:41:19 +1100

    Create RegEdit_Change.ps1

---
+++ RegEdit_Change.ps1
@@ -0,0 +1,27 @@
+param (
+    [switch]$Off
+)
+#Variables
+$ValueOn         = 'VoiceAccess'
+$ValueOff        = $null
+$Path            = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Accessibility'
+$Name            = 'Configuration'
+$PropertyType    = 'String'
+$PathStart       = Split-Path -Path $Path -Parent
+$PathEnd         = Split-Path -Path $Path -Leaf
+#Unless we're turning it off
+if ($Off) {
+    $Value       = $ValueOff
+} else {
+    $Value       = $ValueOn
+}
+#Test for existing value
+$GetVal          = Get-ItemPropertyValue -Path $Path -Name $Name
+if (!($GetVal)) {
+    New-Item -Path $PathStart -Name $PathEnd -Force
+    New-ItemProperty -Path $Path -Name $Name -PropertyType $PropertyType -Value $Value
+} elseif ($GetVal -ne $Value) {
+    Set-ItemProperty -Name $Name -Path $Path -Value $Value
+} else {
+    exit 0
+}

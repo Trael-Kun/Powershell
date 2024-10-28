3160fe90923b2f0bc4d3be3d8c1a98e7df54cc55
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-10-28 15:25:34 +1100

    Update RegKey-CREATE.ps1

--- intune/Remediation/RegKey-CREATE.ps1
+++ intune/Remediation/RegKey-CREATE.ps1
@@ -1,2 +1,29 @@
-New-Item -Path "HKCU:\Software\Micro Focus\Content Manager" -Name OfficeAddins -Force
-New-ItemProperty -Path "HKCU:\Software\Micro Focus\Content Manager\OfficeAddins" -Name UseNativeUI -PropertyType DWord -Value 1
+param (
+    [switch]$Off
+)
+
+$ValueOn         = 'VoiceAccess'
+$ValueOff        = ''
+$Path            = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Accessibility'
+$Name            = 'Configuration'
+$PropertyType    = 'String'
+$PathStart       = Split-Path -Path $Path -Parent
+$PathEnd         = Split-Path -Path $Path -Leaf
+
+#Unless we're turning it off
+if ($Off) {
+    $Value       = $ValueOff
+} else {
+    $Value       = $ValueOn
+}
+
+$GetVal          = Get-ItemPropertyValue -Path $Path -Name $Name
+
+if (!($GetVal)) {
+    New-Item -Path $PathStart -Name $PathEnd -Force
+    New-ItemProperty -Path $Path -Name $Name -PropertyType $PropertyType -Value $Value
+} elseif ($GetVal -ne $Value) {
+    Set-ItemProperty -Name $Name -Path $Path -Value $Value
+} else {
+    exit 0
+}

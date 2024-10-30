a57ef0ec6b3ee272666c9206dd0c1b3280cb75bf
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-10-30 14:25:11 +1100

    Update AutopilotLogging.ps1

--- intune/Enrolment/AutopilotLogging.ps1
+++ intune/Enrolment/AutopilotLogging.ps1
@@ -1,3 +1,28 @@
+<#
+.SYNOPSIS
+    Logs details of devices for Autopilot enrolments.
+
+.DESCRIPTION
+    Logs device details, designed to aid Autopilot enrolment as part of an SCCM task sequence.
+    Logs the following;
+        - Time Script was run (Local, UTC & notes time difference)
+        - Device Name
+        - Device Serial Number
+        - Device Manufacturer
+        - Device Model
+        - BIOS Version
+        - Intune Group Tag
+        - Total C: Drive Size
+        - Free C: Drive Space
+        - User accounts present on device
+
+.NOTES
+    Author: Bill Wilson
+    Date:   25/10/2024
+#>
+param (
+    [switch]$Wmi #gathers device info from wmi instead of TS variables
+)
 function Write-Log {
     param (
         [Parameter(Mandatory=$true)]
@@ -11,7 +36,7 @@ function Write-Log {
             Add-Content -Path $LogFile -Value "$Message" -Force
         
         } else {
-            $DateTime = [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
+            $DateTime   = [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
             Write-Output $Message
             Add-Content -Path $LogFile -Value "$DateTime	|	$Message"  -Force
         }
@@ -20,72 +45,89 @@ function Write-Log {
         Write-Output $Message
     }
 }
-
-$Domain         = 'ReallyRealCompany.com'
-
-##Gather Info
-#Task sequence variables
-$tsenv  = New-Object -COMObject Microsoft.SMS.TSEnvironment
-$GroupTag       = $tsenv.Value('GroupTag')
-$LogFile        = $tsenv.Value('LogFile')
-$PcName         = $tsenv.Value('_SMSTSMachineName')
-$Serial         = $tsenv.Value('Serial')
-$Manufacturer   = $tsenv.Value('Manufacturer')
-$Model          = $tsenv.Value('Model')
-$BiosVer        = $tsenv.Value('BiosVer')
-
-#Drives
-$Drive = Get-WmiObject -Class Win32_LogicalDisk -ComputerName LOCALHOST | Where-Object {$_. DeviceID -eq 'C:'} | Select-Object DeviceID, {[int]($_.Size /1GB)}, {[int]($_.FreeSpace /1GB)}
-
-#Time Variables
-$LocalTime =    Get-Date -Format "yyyy-MM-dd HH:mm:ss LOCAL"
-$UtcTime =      [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
-$UtcOffset =    Get-Date -Format "%K"
-
-#Users
-$UserList = @()
-$Users = (Get-ChildItem $env:SystemDrive\Users).name
-foreach ($User in $Users) {
-    $ThisUser = [adsisearcher]"(samaccountname=$User)"
-    $PrincipalName = ($ThisUser.FindOne().Properties.UserPrincipalName)
-    if ($PrincipalName -like "*@$Domain") {
-        $UserList += $PrincipalName
-        #$ThisUser.FindOne().Properties.mail
+function Test-Variable {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$VarVal,
+        [string]$VarName
+    )
+    if ($VarVal) {
+        Write-Log -NoDate "$VarName`:	$VarVal"
     } else {
-        Write-Host "$PrincipalName cannot be a primary account"
+        Write-Log -NoDate "$VarName`:	NOT FOUND"
     }
 }
 
-#Does Grouptag exist?
-if ($null -eq $GroupTag -or $GroupTag -eq "") {
-    $TagTest    = $false
-} else {
-    $TagTest    = $true
-}
+$Domain       = 'company.com'
+
+#Task sequence variables
+$tsenv  = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$LogFile      = $tsenv.Value('LogFile')
 
 # Write to log
-Write-Log -NoDate "AutoPilot Hardware Hash Task Sequence"
-Write-Log -NoDate "Local Time:		$LocalTime"
-Write-Log -NoDate "UTC Time:		$UtcTime"
-Write-Log -NoDate "Offset:			$UtcOffset"
-Write-Log -NoDate ""
-Write-Log -NoDate "##Device Details"
-Write-Log -NoDate "Device Name:		$PcName"
-Write-Log -NoDate "Serial No:		$Serial"
-Write-Log -NoDate "Make:			$Manufacturer"
-Write-Log -NoDate "Model:			$Model"
-Write-Log -NoDate "BIOS Version:	$BiosVer"
-if ($TagTest) {
-    Write-Log -NoDate "Group Tag:		$GroupTag"
+Write-Log 'START'
+Write-Log -NoDate ''
+Write-Log -NoDate 'AutoPilot Hardware Hash Task Sequence'
+#Time Variables
+$LocalTime    = Get-Date -Format "yyyy-MM-dd HH:mm:ss LOCAL"
+Write-Log -NoDate "Local Time:	$LocalTime"
+$UtcTime      = [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
+Write-Log -NoDate "UTC Time:	$UtcTime"
+$UtcOffset    = Get-Date -Format "%K"
+Write-Log -NoDate "Offset:	$UtcOffset"
+Write-Log -NoDate ''
+
+#Device Info
+Write-Log -NoDate '##Device Details'
+if ($Wmi) {
+    $Bios         = Get-CimInstance -Class Win32_Bios
+    $Sys          = Get-CimInstance -Class Win32_ComputerSystem
+    $PcName       = $Sys.Name
+    $Serial       = $Bios.SerialNumber
+    $Manufacturer = $Sys.Manufacturer
+    $BiosVer      = $Bios.SMBIOSBIOSVersion
+    $Model        = $Sys.Model
+    $GroupTag     = $tsenv.Value('GroupTag')
 } else {
-    Write-Log -NoDate "Group Tag:		N/A"
+    $PcName       = $tsenv.Value('_SMSTSMachineName')
+    $Serial       = $tsenv.Value('Serial')
+    $Manufacturer = $tsenv.Value('Manufacturer')
+    $Model        = $tsenv.Value('Model')
+    $BiosVer      = $tsenv.Value('BiosVer')
+    $GroupTag     = $tsenv.Value('GroupTag')
 }
-Write-Log -NoDate ""
-Write-Log -NoDate "##Drive Info"
-Write-Log -NoDate "Drive Size:		$($Drive.'[int]($_.Size /1GB)')GB"
-Write-Log -NoDate "Free Space:		$($Drive.'[int]($_.FreeSpace /1GB)')GB"
-Write-Log -NoDate ""
-Write-Log -NoDate "##User List"
+Test-Variable -VarVal $PcName       -VarName 'Device Name'
+Test-Variable -VarVal $Serial       -VarName 'Serial No'
+Test-Variable -VarVal $Manufacturer -VarName 'Make'
+Test-Variable -VarVal $Model        -VarName 'Model'
+Test-Variable -VarVal $BiosVer      -VarName 'BIOS Version'
+Test-Variable -VarVal $GroupTag     -VarName 'Group Tag'
+Write-Log -NoDate ''
+
+#Drives
+Write-Log -NoDate '##Drive C: Info'
+$Drive        = Get-CimInstance -Class Win32_LogicalDisk -ComputerName LOCALHOST | Where-Object {$_. DeviceID -eq 'C:'} | Select-Object {[int]($_.Size /1GB)}, {[int]($_.FreeSpace /1GB)}
+Test-Variable -VarVal "$($Drive.'[int]($_.Size /1GB)')GB"       -VarName 'Drive Size'
+Test-Variable -VarVal "$($Drive.'[int]($_.FreeSpace /1GB)')GB"  -VarName 'Free Space'
+Write-Log -NoDate ''
+
+#Users
+$UserList     = @()
+$Users        = (Get-ChildItem $env:SystemDrive\Users).Name
+foreach ($User in $Users) {
+    $ThisUser       = [adsisearcher]"(samaccountname=$User)"
+    $PrincipalName  = ($ThisUser.FindOne().Properties.UserPrincipalName)
+    if ($PrincipalName -like "*@$Domain") {
+        $UserList   += $PrincipalName
+        Write-Host "$PrincipalName added" -ForegroundColor Green
+    } else {
+        Write-Host "$PrincipalName cannot be a primary account" -ForegroundColor Red -BackgroundColor Black
+    }
+}
+Write-Log     -NoDate '##User List'
 foreach ($Principal in $UserList) {
-    Write-Log -NoDate "User:			$Principal"
+    Test-Variable -VarVal $Principal    -VarName 'User'
 }
+Write-Log -NoDate ''
+
+Write-Log 'END'

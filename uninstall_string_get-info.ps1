1890e45e69314ee950a984b1469d1e2d5daf5eec
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-10-31 17:10:21 +1100

    Update uninstall_string_get-info.ps1

--- uninstall_string_get-info.ps1
+++ uninstall_string_get-info.ps1
@@ -12,6 +12,8 @@ $ScriptName = $MyInvocation.MyCommand.Name
 
  02/03/2023 Bill Wilson     Added WOW6432Node reg key to list
  30/10/2024 Bill Wilson     Added HKU hive
+ 31/10/2024 Bill Wilson     Added User & Architecture functions
+                            Changed final formatting
 #>
 
 Write-Host "`r`n$ScriptPath\$ScriptName" -ForegroundColor Yellow
@@ -21,40 +23,65 @@ Write-Host ""
 
 ## Set Variables
 ###################
-# Name of PC
-$PCName = $env:computername
-# Define where output file is stored
-$logdir = "c:\temp\logs"
-# Name output file
-$logfile  = "$PCname-uninstalls_$(get-date -format yymmdd_hhmmtt).csv"
-# create log directory
-if (Test-Path -Path $logdir){
-    Write-Host "Logging to $logdir"
-} else {
-    New-Item -ItemType "directory" -Path $logdir -Force
-    Write-Host "$logdir created"
-}
+$PCName = $env:computername                                             #Name of PC
+$LogDir = "c:\temp\logs"                                                #where output file is stored
+$LogFile  = "$PCname-uninstalls_$(get-date -format yymmdd_hhmmtt).csv"  # Name output file
 
-$StdPath = 'Microsoft\Windows\CurrentVersion\Uninstall'
+$Uninst = @()   #empty array for final output
+$StdPath = 'Microsoft\Windows\CurrentVersion\Uninstall' #common reg path
 $RegPaths = @( "HKLM:\SOFTWARE\$StdPath",
 "HKLM:\SOFTWARE\Wow6432Node\$StdPath",
 "Registry::HKU\*\Software\$StdPath",
 "Registry::HKU\*\Software\Wow6432Node\$StdPath"
 )
+
+# create log directory
+if (Test-Path -Path $LogDir){
+    Write-Host "Logging to $LogDir"
+} else {
+    New-Item -ItemType "directory" -Path $LogDir -Force
+    Write-Host "$LogDir created"
+}
+
+# Get list of users & corresponding SIDs
+$SidList = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" | Get-ItemProperty | Select-Object PSChildNamename,ProfileImagePath
+foreach ($Sid in $SidList) {
+    if ($Sid.ProfileImagePath -like "$env:Systemdrive\Users\*") {   
+        $SidUser = ((Split-Path -Path $Sid -Leaf).Replace('; username=}',''))
+        $Object = @(
+            [pscustomobject]@{PSChildname = $Sid.PSChildName; Username = $SidUser}
+        )
+        $Users += $Object
+    }
+}
+
 ## Get Uninstall Strings
-$Uninst = Get-ChildItem -Path $RegPaths | Get-ItemProperty | Select-Object -Property DisplayName, Publisher, DisplayVersion, UninstallString
-#$wmiproperties = Get-CimInstance -Query "SELECT ProductCode,Value FROM Win32_Property WHERE Property='UpgradeCode'"
-
-# Set Data Table
-<#$packageinfo = New-Object System.Data.Datatable
-[void]$packageinfo.Columns.Add("DisplayName")
-[void]$packageinfo.Columns.Add("DisplayVersion")
-[void]$packageinfo.Columns.Add("Publisher")
-[void]$packageinfo.Columns.Add("UninstallString")
-#>
-
-# Format Output
-#$packageinfo | Sort-Object -Property DisplayName | Format-table DisplayName, DisplayVersion, Publisher, UninstallString
+$Apps = Get-ChildItem -Path $RegPaths -ErrorAction SilentlyContinue| Get-ItemProperty | Select-Object -Property DisplayName, Publisher, DisplayVersion, UninstallString, PSPath
+foreach ($App in $Apps) {
+    if (($null -ne $App.UninstallString) -or ($App.UninstallString -ne "")) {
+        if ($App.PSPath -like "*\Wow6432Node\*") { #32 or 64-bit?
+            $Arch = 'x86'
+        } else {
+            $Arch = 'x64'
+        }
+        #Is it a machine-wide install, or only for user?
+        if ($App.PSPath -like "*HKEY_LOCAL_MACHINE\*") {
+            $User = 'HKLM'
+        } else {
+            foreach ($Sid in $Users) {  #check SIDs against usernames
+                if ($App.PSPath -like $Sid.PSChildName) {
+                    $User = $Sid.UserName
+                }
+            }
+        }
+    }
+    $Collation = @(                 #format for table
+        [pscustomobject]@{DisplayName=$App.DisplayName; Publisher=$App.Publisher; Version=$App.DisplayVersion; Architecture=$Arch; User=$User; Uninstall=$App.UninstallString}
+        )
+    if ($Collation.Uninstall) {     #only gather info that's useful
+        $Uninst += $Collation       #add info to final output
+    }
+}
 
 ## Summary
 #count
@@ -62,15 +89,14 @@ $installed = $Uninst.Count
 Write-Host "Found $installed Uninstall Strings"
 
 # Export info
-Write-Host "Logging to $logdir"
-$Uninst | Export-Csv $logdir$logfile
+Write-Host "Logging to $LogDir"
+$Uninst | Export-Csv (Join-Path -Path $LogDir -ChildPath $LogFile)
 
 # Tell User where log is
-if (Test-Path -Path $logdir\$logfile){
+if (Test-Path -Path (Join-Path -Path $LogDir -ChildPath $LogFile)) {
     Write-Host "Logged to " -NoNewline
-    Write-Host "$logdir$logfile`r`n" -ForegroundColor Green
-    }
-else {
+    Write-Host "$LogDir\$LogFile`r`n" -ForegroundColor Green
+} else {
     Write-Host "Failed to write .csv file`r`n" -ForegroundColor Red
-    }
+}
 # End Script

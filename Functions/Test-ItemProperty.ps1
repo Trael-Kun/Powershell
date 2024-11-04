b29a2b2294520077b0b54b47dc9175b66866649f
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-04 14:56:19 +1100

    Update Test-ItemProperty.ps1
    added notes

--- Functions/Test-ItemProperty.ps1
+++ Functions/Test-ItemProperty.ps1
@@ -1,4 +1,11 @@
 function Test-ItemProperty {
+ <#
+    .SYNOPSIS
+        Automatically checks for the existence of a file property. Useful for checking registry.
+    .NOTES
+        Author: Bill Wilson
+        Date 04/11/2024
+    #>
   param (
     [string]$Path,
     [string]$PropertyName

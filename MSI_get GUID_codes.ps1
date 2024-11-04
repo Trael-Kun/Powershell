5321d18a94c1404cc4ba3b9fa08be80c03097330
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-04 16:01:56 +1100

    Update MSI_get GUID_codes.ps1
    tidied formatting

--- MSI_get GUID_codes.ps1
+++ MSI_get GUID_codes.ps1
@@ -43,6 +43,7 @@ $ScriptName = $MyInvocation.MyCommand.Name
  Modified by Bill 30/08/2023; Added "PC" to PCname options (inserts local PC name)
  Modified by Bill 22/11/2023; Added extra check to remove ping on local PC
  Modified by Bill 29/11/2023; Refined local PC check
+ Modified by Bill 04/11/2024; Tidied formatting
 #>
 
 # Opening Descriptive with box
@@ -64,188 +65,173 @@ Write-Host ""
 
 ## Start Script
 DO {
-        ## Set Variables
-        #################
+    ## Set Variables
+    #################
+    # Asset No. Input
+    Write-Host " " -NoNewline
+    Write-Host ' input target PC Name or WKS Asset No. (or' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
+    Write-Host ' EXIT' -NoNewline -ForegroundColor Yellow -BackgroundColor Black
+    Write-Host ' to stop) :' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
+    Write-Host ' ' -NoNewline
+    $Asset = Read-Host
 
-        # Asset No. Input
+    ## Set Computer Name
+    ####################
+    # Add prefix to asset no.
+    if ($Asset -eq "") {
+        Write-Host ""
         Write-Host " " -NoNewline
-        Write-Host ' input target PC Name or WKS Asset No. (or' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
-        Write-Host ' EXIT' -NoNewline -ForegroundColor Yellow -BackgroundColor Black
-        Write-Host ' to stop) :' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
-        Write-Host ' ' -NoNewline
-        $Asset = Read-Host
-
-        ## Set Computer Name
-        ####################
-        # Add prefix to asset no.
-        if ($Asset -eq "") {
-            Write-Host ""
+        Write-Host " No name or asset entered " -ForegroundColor White -BackgroundColor Red
+        Write-Host ""
+    } else {
+        if ($Asset -match 'exit') {
+            exit
+        } elseif ($Asset -eq 'o') {
             Write-Host " " -NoNewline
-            Write-Host " No name or asset entered " -ForegroundColor White -BackgroundColor Red
-            Write-Host ""
+            Write-Host ' OVERRIDE MODE ' -ForegroundColor Red -BackgroundColor Yellow
+            Write-Host " " -NoNewline
+            Write-Host ' input target PC Name :' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
+            Write-Host ' ' -NoNewline
+            $PCname = Read-Host
+        } elseif ($Asset -match 'PC') {
+            $PCname = "$env:ComputerName"
+        } elseif ($Asset -match '^WKS') {
+            $PCname = "$Asset"
+        } elseif ($Asset -notmatch '^WKS') {
+            $PCname = "WKS$Asset"
         }
-        else {
-            if ($Asset -match 'exit') {
-                EXIT
-            }
-            elseif ($Asset -eq 'o') {
-                Write-Host " " -NoNewline
-                Write-Host ' OVERRIDE MODE ' -ForegroundColor Red -BackgroundColor Yellow
-                Write-Host " " -NoNewline
-                Write-Host ' input target PC Name :' -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
-                Write-Host ' ' -NoNewline
-                $PCname = Read-Host
-            }
-            elseif ($Asset -match 'PC') {
-                $PCname = "$env:ComputerName"
-            }
-            elseif ($Asset -match '^WKS') {
-                $PCname = "$Asset"
-            }
-            elseif ($Asset -notmatch '^WKS') {
-                $PCname = "WKS$Asset"
-            }
-            # Define where output file is stored
-            $locallogdir = "$env:SystemDrive\temp\logs\"
-            $targlogdir = "\\$PCname\c$\temp\logs\"
-            # Name output file
-            $logfile  = "$PCname-MSI-codes_$(get-date -format yymmdd_hhmmtt).csv"
+        # Define where output file is stored
+        $locallogdir = "$env:SystemDrive\temp\logs"
+        $targlogdir = "\\$PCname\c$\temp\logs"
+        # Name output file
+        $logfile  = "$PCname-MSI-codes_$(get-date -format yymmdd_hhmmtt).csv"
+        $LocalLog = Join-Path -Path $locallogdir -ChildPath $logfile
+        $targlog  = Join-Path -Path $targlogdir -ChildPath $logfile
 
-            ######################
-            ## Check if PC is online
-            if ($PCname -eq $env:ComputerName) {
-                $PCping = $true
-           }
-           else {
-                $PCping = Test-Connection -ComputerName $PCname -Count 1 -Quiet
-           }
-        
-            if ($PCping -eq $true) {
-
-                ## Get data
-                ###################
-                # Get Install Info
-                Write-Host " Getting MSI GUIDs for " -NoNewline
-                Write-Host "$PCname" -NoNewLine -ForegroundColor Green
+        ######################
+        ## Check if PC is online
+        if ($PCname -eq $env:ComputerName) {
+            $PCping = $true
+        } else {
+            $PCping = Test-Connection -ComputerName $PCname -Count 1 -Quiet
+        }
     
-                # Check if local or remote PC
-                $Step = 0
-                Write-Progress -Activity 'Gathering GUIDs' -Status 'Getting MSI info' -PercentComplete ($Step++)
-                if ($PCname -ne $env:computername) {
-                    #$wmipackages = Get-CimInstance -Class win32_product -ComputerName $PCname
-                    $wmipackages = Get-WmiObject -Class win32_product -ComputerName $PCname
-                }
-                else {
-                    $wmipackages = Get-CimInstance -Class win32_product
-                    #$wmipackages = Get-WmiObject -Class win32_product -ComputerName $PCname
-                }
+        if ($PCping -eq $true) {
 
-                # Select relevant values
-                Write-Progress -Activity 'Gathering GUIDs' -Status 'Filtering Values' -PercentComplete ((($Step++) / $wmipackages.count) *100)
-                $wmiproperties = Get-CimInstance -Query "SELECT ProductCode,Value FROM Win32_Property WHERE Property='UpgradeCode'"
-    
-                # create log directory
-                if ($PCname -ne $env:ComputerName) {
-                    if (Test-Path -Path $targlogdir) {
-                        Write-Host "Logging to $targlogdir"
-                        }
-                    else {
-                        New-Item -ItemType "directory" -Path $targlogdir -Force
-                        Write-Host "$targlogdir created"
-                        Write-Host "Logging to $targlogdir"
-                        New-Item -ItemType "directory" -Path $locallogdir -Force
-                        Write-Host "$locallogdir created"
-                        Write-Host "Logging to $locallogdir"
+            ## Get data
+            ###################
+            # Get Install Info
+            Write-Host " Getting MSI GUIDs for " -NoNewline
+            Write-Host "$PCname" -NoNewLine -ForegroundColor Green
+
+            # Check if local or remote PC
+            $Step = 0
+            Write-Progress -Activity 'Gathering GUIDs' -Status 'Getting MSI info' -PercentComplete ($Step++)
+            if ($PCname -ne $env:computername) {
+                #$wmipackages = Get-CimInstance -Class win32_product -ComputerName $PCname
+                $wmipackages = Get-WmiObject -Class win32_product -ComputerName $PCname
+            } else {
+                $wmipackages = Get-CimInstance -Class win32_product
+                #$wmipackages = Get-WmiObject -Class win32_product -ComputerName $PCname
+            }
+
+            # Select relevant values
+            Write-Progress -Activity 'Gathering GUIDs' -Status 'Filtering Values' -PercentComplete (($Step++ / $wmipackages.count) * 100)
+            $wmiproperties = Get-CimInstance -Query "SELECT ProductCode,Value FROM Win32_Property WHERE Property='UpgradeCode'"
+
+            # create log directory
+            if ($PCname -ne $env:ComputerName) {
+                if (Test-Path -Path $targlogdir) {
+                    Write-Host "Logging to $targlogdir"
+                    } else {
+                    New-Item -ItemType "directory" -Path $targlogdir -Force
+                    Write-Host "$targlogdir created"
+                    Write-Host "Logging to $targlogdir"
+                    New-Item -ItemType "directory" -Path $locallogdir -Force
+                    Write-Host "$locallogdir created"
+                    Write-Host "Logging to $locallogdir"
+                }
+            } else {
+                if (Test-Path -Path $locallogdir) {
+                    Write-Host "Logging to $locallogdir"
+                } else {
+                    New-Item -ItemType "directory" -Path $locallogdir -Force
+                    Write-Host "$locallogdir created"
+                    Write-Host "Logging to $locallogdir"
+                }
+            }
+            
+            # Set Data Table
+            $packageinfo = New-Object System.Data.Datatable
+            [void]$packageinfo.Columns.Add("Name")
+            [void]$packageinfo.Columns.Add("ProductCode")
+            [void]$packageinfo.Columns.Add("UpgradeCode")
+            [void]$packageinfo.Columns.Add("Version")
+            [void]$packageinfo.Columns.Add("Vendor")
+
+            # add blanks for no upgrade code
+            foreach ($package in $wmipackages) {
+                Write-Progress -Activity 'Gathering GUIDs' -Status 'Formatting Output' -PercentComplete (($Step++ / $wmipackages.count) * 100)
+                $foundupgradecode = $false #Assume no upgrade code is found
+                foreach ($property in $wmiproperties) {
+                    if ($package.IdentifyingNumber -eq $property.ProductCode) {
+                        [void]$packageinfo.Rows.Add($package.Name,$package.IdentifyingNumber, $property.Value, $package.Version, $package.Vendor)
+                        $foundupgradecode = $true
+                        break
                     }
                 }
-                else {
-                    if (Test-Path -Path $locallogdir) {
-                        Write-Host "Logging to $locallogdir"
-                    }
-                    else {
-                        New-Item -ItemType "directory" -Path $locallogdir -Force
-                        Write-Host "$locallogdir created"
-                        Write-Host "Logging to $locallogdir"
-                    }
-                }
-                
+                if(-Not ($foundupgradecode)) { 
+                # No upgrade code found, add product code to list
+                [void]$packageinfo.Rows.Add($package.Name,$package.IdentifyingNumber, "") 
+                }   
+            }
 
-                # Set Data Table
-                $packageinfo = New-Object System.Data.Datatable
-                [void]$packageinfo.Columns.Add("Name")
-                [void]$packageinfo.Columns.Add("ProductCode")
-                [void]$packageinfo.Columns.Add("UpgradeCode")
-                [void]$packageinfo.Columns.Add("Version")
-                [void]$packageinfo.Columns.Add("Vendor")
+            ## Format Output
+            Write-Progress -Activity 'Gathering GUIDs' -Status 'Formatting Output'  -PercentComplete 85
+            $packageinfo | Sort-Object -Property Name | Format-table ProductCode, Name, Vendor, Version, UpgradeCode
 
-                # add blanks for no upgrade code
-                foreach ($package in $wmipackages) {
-                    Write-Progress -Activity 'Gathering GUIDs' -Status 'Formatting Output' -PercentComplete ((($Step++) / $wmipackages.count) *100)
-                    $foundupgradecode = $false #Assume no upgrade code is found
-                    foreach ($property in $wmiproperties) {
-                        if ($package.IdentifyingNumber -eq $property.ProductCode) {
-                            [void]$packageinfo.Rows.Add($package.Name,$package.IdentifyingNumber, $property.Value, $package.Version, $package.Vendor)
-                            $foundupgradecode = $true
-                            break
-                        }
-                    }
-                    if(-Not ($foundupgradecode)) { 
-                    # No upgrade code found, add product code to list
-                    [void]$packageinfo.Rows.Add($package.Name,$package.IdentifyingNumber, "") 
-                    }   
-                }
- 
-                ## Format Output
-                Write-Progress -Activity 'Gathering GUIDs' -Status 'Formatting Output'  -PercentComplete 90
-                $packageinfo | Sort-Object -Property Name | Format-table ProductCode, Name, Vendor, Version, UpgradeCode
+            # Export to log
+            Write-Progress -Activity 'Gathering GUIDs' -Status 'Logging Output' -PercentComplete 100
+            $packageinfo | Export-Csv $targlog
+            if ($PCname -ne $env:ComputerName){
+                $packageinfo | Export-Csv $targlog
+                $packageinfo | Export-Csv "$locallog"
+            } else {
+                $packageinfo | Export-Csv "$locallog"
+            }
 
-                # Export to log
-                Write-Progress -Activity 'Gathering GUIDs' -Status 'Logging Output' -PercentComplete 100
-                $packageinfo | Export-Csv $targlogdir$logfile
-                if ($PCname -ne $env:ComputerName){
-                    $packageinfo | Export-Csv $targlogdir$logfile
-                    $packageinfo | Export-Csv "$locallogdir$logfile"
-                }
-                else {
-                    $packageinfo | Export-Csv "$locallogdir$logfile"
-                }
+            ## Summary
+            ############
+            #count
+            $installed = $wmipackages.Count
+            Write-Host "Found $installed GUIDs"
 
-                ## Summary
-                ############
-                #count
-                $installed = $wmipackages.Count
-                Write-Host "Found $installed GUIDs"
-
-                # Tell User where log is
-                if (Test-Path -Path "$targlogdir$logfile") {
-                    if (Test-Path -path "$locallogdir$logfile") {
-                        Write-Host "Logged to " -NoNewline
-                        Write-Host "$targlogdir$logfile`r`n" -ForegroundColor Green
-                        Write-Host "Logged to " -NoNewline
-                        Write-Host "$locallogdirlogdir$logfile`r`n" -ForegroundColor Green
-                    }
-                    else {
+            # Tell User where log is
+            if (Test-Path -Path "$targlog") {
+                if (Test-Path -path "$locallog") {
                     Write-Host "Logged to " -NoNewline
-                    Write-Host "$targlogdir$logfile`r`n" -ForegroundColor Green
-                       }
-                }
-                elseif (Test-Path -Path "$locallogdir$logfile"){
+                    Write-Host "$targlog`r`n" -ForegroundColor Green
                     Write-Host "Logged to " -NoNewline
-                    Write-Host "$locallogdir$logfile`r`n" -ForegroundColor Green
+                    Write-Host "$locallogdirlogdir$logfile`r`n" -ForegroundColor Green
+                } else {
+                Write-Host "Logged to " -NoNewline
+                Write-Host "$targlog`r`n" -ForegroundColor Green
                 }
-                else {
-                    Write-Host "Failed to write .csv file`r`n" -ForegroundColor Red
-                }
+            } elseif (Test-Path -Path "$locallog"){
+                Write-Host "Logged to " -NoNewline
+                Write-Host "$locallog`r`n" -ForegroundColor Green
+            } else {
+                Write-Host "Failed to write .csv file`r`n" -ForegroundColor Red
+            }
             Write-Progress -Activity 'Gathering GUIDs' -Status 'Complete' -PercentComplete 100
-            }
-            else {
+        } else {
             Write-Host " "
             Write-Host " " -NoNewline
             Write-Host " Target computer" -ForegroundColor White -BackgroundColor Red -NoNewline
             Write-Host " $PCname" -ForegroundColor Green -BackgroundColor Red -NoNewline
             Write-Host " unavailable! " -ForegroundColor White -BackgroundColor Red
             Write-Host " "
-            }
         }
+    }
 } while ($true -eq $true)
 ## End Script

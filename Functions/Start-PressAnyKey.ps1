a3158f47be8f56f173d79ec5da757089a3920626
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-05 15:59:28 +1100

    Create Start-PressAnyKey.ps1

---
+++ Functions/Start-PressAnyKey.ps1
@@ -0,0 +1,22 @@
+Function Get-Wait {
+  <#
+  .SYNOPSIS
+   Pauses script for user interaction
+  .NOTES
+   Thief:  Bill Wilson 
+   Date:   05/11/2024
+   
+   stolen wholesale from;
+   https://stackoverflow.com/questions/20886243/press-any-key-to-continue
+  #>
+  param (
+    [string]$Message
+    # Check if running Powershell ISE
+    if ($psISE) {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    } else {
+        Write-Host "$Message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}

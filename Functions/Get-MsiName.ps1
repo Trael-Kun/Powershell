450e74f261c2ece54ed093ed5ff33a5991128e91
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-06 11:29:53 +1100

    Create Get-MsiName.ps1

---
+++ Functions/Get-MsiName.ps1
@@ -0,0 +1,17 @@
+function Get-MsiName {
+    <#
+        GUID Detection
+        Author: Bill Wilson (https://github.com/Trael-Kun)
+        Date:   06/11/2024
+    #>
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Name
+    )
+    if (Get-CimInstance -Class win32_product | Where-Object{$_.Name -eq $Name}) {
+        exit 0
+    } else {
+        exit 1
+    }
+}

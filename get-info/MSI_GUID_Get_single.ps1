ccc9d35226daea61d9570107bc54146cdb6e76d0
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-06 13:18:45 +1100

    Update MSI_GUID_Get_single.ps1
    added comments & param help

--- get-info/MSI_GUID_Get_single.ps1
+++ get-info/MSI_GUID_Get_single.ps1
@@ -1,22 +1,39 @@
 <#
+.SYNOPSIS
     get an MSI GUID fast
+.NOTES    
     Author: Bill Wilson (https://github.com/Trael-Kun)
     Date:   06/11/2024
+    References;
+        https://www.sapien.com/blog/2014/11/18/removing-objects-from-arrays-in-powershell/
+        https://www.tutorialspoint.com/how-to-retrieve-the-msi-package-product-code-using-powershell
 #>
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$Name
+    [Parameter(Mandatory=$true,
+        HelpMessage='Name of the program to search. Accepts * as a wildcard.')]
+    [string]$Name,
+    [Parameter(Mandatory=$false,
+        HelpMessage='Switch to activate "Do You Want To Install" prompt for absent providers')]
+    [switch]$Install
 )
+# create empty array
 $ProviderNames = @()
+# List providers you want to search, comment out unneeded
 $Providers = (
     'msi',
-    'msu',
+    #'msu',
     'chocolatey',
     'nuget',
     'PowerShellGet',
-    'psl',
+    #'psl',
     'Programs'
 )
+# get installed providers
 $ProviderList = (Get-PackageProvider).Name
-$ProviderNames = $Providers | Where-Object { $_ -in $ProviderList }
+# 
+if ($Install) {
+    $ProviderNames = $ProviderList
+} else {
+    $ProviderNames = $Providers | Where-Object { $_ -in $ProviderList }
+}
 Get-Package -ProviderName $ProviderNames -Name $Name | Select-Object -Property Name,Version,FastPackageReference

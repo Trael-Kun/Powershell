012dd0fb69cdc163244e50d383d06ff73fb0ea99
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-06 11:28:10 +1100

    Update and rename MsiGuid_DETECT.ps1 to Get-MsiGuid.ps1

---
+++ Functions/Get-MsiGuid.ps1
@@ -0,0 +1,17 @@
+function Get-MsiGuid {
+    <#
+        GUID Detection
+        Author: Bill Wilson (https://github.com/Trael-Kun)
+        Date:   06/11/2024
+    #>
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Guid
+    )
+    if (Get-CimInstance -Class win32_product | Where-Object{$_.IdentifyingNumber -eq $Guid}) {
+        exit 0
+    } else {
+        exit 1
+    }
+}

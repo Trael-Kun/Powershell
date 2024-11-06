be6de074f0cba27492ec797baea3debe5943d12d
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-06 12:45:55 +1100

    Update MSI_GUID_Get_single.ps1

--- get-info/MSI_GUID_Get_single.ps1
+++ get-info/MSI_GUID_Get_single.ps1
@@ -4,15 +4,23 @@
     Date:   06/11/2024
 #>
 param (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Name
-    )
-$Providers = ('msi',
-    #'msu',
-    #'chocolatey',
-    #'nuget",
+    [Parameter(Mandatory=$true)]
+    [string]$Name
+)
+$ProviderNames = @()
+$Providers = (
+    'msi',
+    'msu',
+    'chocolatey',
+    'nuget',
     'PowerShellGet',
     'psl',
-    'Programs')
-(Get-Package -ProviderName $Providers -Name $Name).FastPackageReference
+    'Programs'
+)
+$ProviderList = (Get-PackageProvider).Name
+foreach ($Provider in $Providers) {
+    if ($Provider -in $ProviderList) {
+        $ProviderNames = $ProviderNames += $Provider
+    }
+}
+Get-Package -ProviderName $ProviderNames -Name $Name | Select-Object -Property Name,Version,FastPackageReference

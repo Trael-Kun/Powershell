55c132691c29f012de68ad5b5d360e851d6558f4
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-06 11:31:59 +1100

    Update Get-MsiGuid.ps1

--- Functions/Get-MsiGuid.ps1
+++ Functions/Get-MsiGuid.ps1
@@ -1,6 +1,6 @@
 function Get-MsiGuid {
     <#
-        GUID Detection
+        find installs per MSI GUID
         Author: Bill Wilson (https://github.com/Trael-Kun)
         Date:   06/11/2024
     #>

69bc14a710ad6aaa8f256f56d3ca2f40c1e4e38e
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-08 14:22:34 +1100

    Create Get-DirectoryDepth.ps1

---
+++ get-info/Get-DirectoryDepth.ps1
@@ -0,0 +1,43 @@
+<#
+ .SYNOPSIS
+    Digs through the target directory and tells you where it is deepest.
+ .PARAMETER TargetDirectory
+    The directory you're targeting.
+ .PARAMETER Report
+    Writes output for each folder tested (Path & depth)
+ .NOTES
+    Author:   Bill Wilson
+    Date:     08/11/24
+    References;
+        https://www.reddit.com/r/PowerShell/comments/w990nh/subfolder_level_depth_count/
+#>
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$TargetDirectory,
+    [Parameter(Mandatory=$false)]
+    [switch]$Report
+)
+#find all the child directories
+$Fetch = Get-ChildItem $TargetDirectory -Directory -Recurse
+#set empty
+$Levels = @()
+#process data
+ForEach ($Level in $Fetch) {
+    #count directories in path
+    [int]$Count = ($Level.FullName.Replace('\\','')).Split('\').Count
+    if ($Report) { #tell us what you're doing
+        Write-Output "Path $($Lev.FullName) is $Count deep"
+    }
+    #build hash table
+    $Levels += [pscustomobject]@{Path=$Level.FullName; Levels=$Count}
+}
+Write-Output ''
+#sort data
+$Deepest = $Levels | Sort-Object -Property Levels | Select-Object -Last 1
+#write results
+Write-Host 'Deepest path is ' -NoNewline
+Write-Host $Deepest.Path -ForegroundColor Green -NoNewline
+Write-Host ' at ' -NoNewline
+Write-Host $Deepest.Levels -ForegroundColor Green -NoNewline
+Write-Host ' levels'
+Write-Output ''

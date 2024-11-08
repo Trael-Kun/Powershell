382c162a4870c06e0616f42b4eeb067edc6b3556
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-08 11:23:37 +1100

    Update Add-IntegerToFileName.ps1
    added variable for leading 0s, added redundancy for file name split

--- Functions/Add-IntegerToFileName.ps1
+++ Functions/Add-IntegerToFileName.ps1
@@ -1,16 +1,24 @@
 function Add-IntegerToFileName {
     <#
     .SYNOPSIS
-        Automatically checks for the existence of a file, and if file is present will append a number to the end of a new filename.
+        Automatically checks for the existence of a file, and if file is present will append a 
+        number to the end of a new filename.
+    .DESCRIPTION
+        Scans destination folder for existing files with the same name, and if found will create 
+        a new name appended with leading zeros (default value is 5). 
     .NOTES
         Author: Bill Wilson
         Date:   29/10/2024
         References;
          https://forums.powershell.org/t/create-filename-incrementing-number/11042/9
          https://collectingwisdom.com/powershell-get-file-extension/
+         https://stackoverflow.com/questions/9788492/powershell-extract-file-name-and-extension
     #>
     param(
-        [string]$OutputFile
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile,
+        [Parameter(Mandatory=$false)]
+        [int]$Zeros
     )
     if ($OutputFile -notlike "*.???") {
         Write-Host ''
@@ -19,18 +27,25 @@ function Add-IntegerToFileName {
         do {
             if (Test-Path $OutputFile) {
                 Write-Output "$OutputFile exists, creating new file."
-            
-                #split the file path into segments
-                $OutputDir      = Split-Path -Path $OutputFile -Parent
-                $OutputFileName = Split-Path -Path $OutputFile -Leaf
-                $FileExt        = [System.IO.Path]::GetExtension($OutputFile)
+                if ($null -eq $Zeros) {
+                    $Zeros = 5
+                }
+                #split the file path into segments for easier processing
+                $OutputFileInfo = Get-Item $OutputFile
+                $OutputDir      = Split-Path -Path $OutputFileInfo.FullName -Parent
+                $OutputFileName = Split-Path -Path $OutputFileInfo.FullName -Leaf
+                $FileExt        = [System.IO.Path]::GetExtension($OutputFileInfo)
                 $OutputFileBase = $OutputFileName.Replace("$FileExt",'')
+                if ($null -eq $FileExt) {   #Redundancy to make sure the above worked (constrained language mode is a @!#?@!)
+                    $FileExt        = $OutputFileInfo.Extension
+                    $OutputFileBase = $OutputFileInfo.BaseName
+                }
             
-                #look for files
+                #look for files (wildcard to look for existing numbered files)
                 $Files = (Get-ChildItem -Path $OutputDir -Filter "$OutputFileBase*$FileExt").FullName
             
                 if ($Files) {
-                    #Report how many files are found, because I'm a grammar nazi
+                    #report how many files are found, because I'm a grammar nazi
                     if ($Files.Count -eq 1) {
                         $Fileses = 'file'
                     } else {
@@ -42,28 +57,28 @@ function Add-IntegerToFileName {
                     $Numbers = ((Split-Path -Path $Files -Leaf).Replace("$FileExt",'')).Replace("$OutputFileBase",'')
                     $Numerous = @()
                     foreach ($Number in $Numbers) {
-                    $Number = $Number.ToString().PadLeft(5,'0')
+                    $Number = $Number.ToString().PadLeft($Zeros,'0')
                     Write-Host "Number is '$number'"
                         if (($null -eq $Number) -or ($Number -eq '')) {
                             $Numerous += '0'
                         } else {
-                            $NumNum = '{0:d5}' -f $Number
+                            $NumNum = "{0:d$Zeros}" -f $Number
                             $Numerous += ($NumNum)
                         }
                     }
                 
-                    #Take the number and then increment by 1
+                    #Take the highest number and then increment by 1
                     [int]$Max = ($Numerous | Sort-Object -Descending | Select-Object -First 1)
                     $Max ++
                     Write-Output "The next number is $Max"
                 
                     #Use padding to pad zeros up to 5 characters
-                    $File = "$OutputFileBase{0}$FileExt" -f $Max.ToString().PadLeft(5,'0')
-                    "Incrementing $Max to generate file $File"
+                    $File = "$OutputFileBase{0}$FileExt" -f $Max.ToString().PadLeft($Zeros,'0')
+                    "Incrementing to $Max`: File will be named $File"
                     $OutputFile = Join-Path -Path $OutputDir -ChildPath $File
                 } 
             } else {
-                Write-Output "$OutputFile not found, OK to create."
+                Write-Host "$OutputFile not found, OK to create." -ForegroundColor Green
             }       
         } until (!(Test-Path $OutputFile))
     } Return $OutputFileNew

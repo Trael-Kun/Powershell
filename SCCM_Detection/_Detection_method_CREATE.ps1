dc41514b19c5f30211c3485f14c6c94a448a90ff
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-11 13:11:55 +1100

    Update _Detection_method_CREATE.ps1

--- SCCM_Detection/_Detection_method_CREATE.ps1
+++ SCCM_Detection/_Detection_method_CREATE.ps1
@@ -1,17 +1,25 @@
-<# Create Detection Method #>
+<# 
+.SYNOPSIS
+    Create Detection Method
+.NOTES
+    Author:    Bill Wilson (https://github.com/Trael-Kun)
+    Date:      11/11/2024
+#>
 
-<#
+$UninstallRegx86    = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+$UninstallRegx64    = $UninstallRegx86.Replace('\Wow6432Node','')
+
 Write-Host "Enter Application Display Name: "
 $DisplayName = Read-Host
 
 ## Registry Detection Method
-    $RegDetect = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -like "$DisplayName" } | Select-Object -Property DisplayName, DisplayVersion, PSChildName
+    $RegDetect = Get-ChildItem -Path $UninstallRegx86,$UninstallRegx64 | Get-ItemProperty | Where-Object {$_.DisplayName -like "$DisplayName" } | Select-Object -Property DisplayName, DisplayVersion, PSChildName
     $RegDetect.DisplayVersion
     $RegDetect.PSChildName
 ## Create Text File with Registry Detection Method
-    $RegFilePath = "C:\Temp\REG_Detection_Method.txt"
+    $RegFilePath = Join-Path -Path $env:SystemDrive -ChildPath 'Temp\REG_Detection_Method.txt'
     New-Item -Path "$RegFilePath" -Force
-    Set-Content -Path "$RegFilePath" -Value "If([Version](Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$($RegDetect.PSChildName)','HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$($RegDetect.PSChildName)' -Name DisplayVersion -ea SilentlyContinue) -ge '$($RegDetect.DisplayVersion)') {"
+    Set-Content -Path "$RegFilePath" -Value "If([Version](Get-ItemPropertyValue -Path '$UninstallRegx64\$($RegDetect.PSChildName)','$UninstallRegx86\$($RegDetect.PSChildName)' -Name DisplayVersion -ea SilentlyContinue) -ge '$($RegDetect.DisplayVersion)') {"
     Add-Content -Path "$RegFilePath" -Value "Write-Host `"Installed`""
     Add-Content -Path "$RegFilePath" -Value "Exit 0"
     Add-Content -Path "$RegFilePath" -Value "}"
@@ -23,7 +31,7 @@ $DisplayName = Read-Host
 ## MSI Detection Method
     $MSIDetect = Get-WMIobject Win32_Product -Filter "name = ""$DisplayName"""
 ## Create Text File with Registry Detection Method
-    $MSIFilePath = "C:\Temp\MSI_Detection_Method.txt"
+    $MSIFilePath = Join-Path -Path $env:SystemDrive -ChildPath 'Temp\MSI_Detection_Method.txt'
     New-Item -Path "$MSIFilePath" -Force
     Set-Content -Path "$MSIFilePath" -Value "If((Get-Wmiobject Win32_Product -filter ""IdentifyingNumber = '$($MSIDetect.IdentifyingNumber)'"") -ge (""Version = '$($MSIDetect.Version)'""))  {"
     Add-Content -Path "$MSIFilePath" -Value "Write-Host `"Installed`""

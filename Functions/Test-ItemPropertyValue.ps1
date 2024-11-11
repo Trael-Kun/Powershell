5624fa118488d73f6611ec8bb1cc427a4a812606
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-11 12:44:20 +1100

    Update Test-ItemPropertyValue.ps1

--- Functions/Test-ItemPropertyValue.ps1
+++ Functions/Test-ItemPropertyValue.ps1
@@ -7,11 +7,11 @@ function Test-ItemPropertyValue {
         Date 04/11/2024
     #>
     param (
-    [Parameter(Mandatory=$true)]
-    [string]$Path,
-    [string]$Name,
-    [Parameter(Mandatory=$false)]
-    [string]$Value
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+        [string]$Name,
+        [Parameter(Mandatory=$false)]
+        [string]$Value
     )
     if ($null -ne $Value) {
         ($null -ne (Get-ItemPropertyValue -Path $Path -Name $Name))

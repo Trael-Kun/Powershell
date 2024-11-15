431ae376327e4a8f964814241d3a25f328711f0a
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-11-15 14:41:36 +1100

    Update printer_add_per_site.ps1
    trimmed it riiiiiiiight down.

--- printer/printer_add_per_site.ps1
+++ printer/printer_add_per_site.ps1
@@ -1,68 +1,41 @@
 <#
 .SYNOPSIS
 Finds all printers on specific print server & installs them
+
 .DESCRIPTION
 Pulls installed printers with Get-printer, them uses the value of \\XXX-PRINT01\FollowMe to
 extract & define office location. Then installs ALL printers from the print server.
+
 .EXAMPLE
 
 .NOTES
-AUTHOR:     Bill
+AUTHOR:     Bill Wilson
 Created:    31/08/2023
+
 References:
 https://learn.microsoft.com/en-us/powershell/module/printmanagement/add-printer
 #>
 
-$ACTprint = 'CBR-PRINT01'
-$NSWprint = 'SYD-PRINT01'
-$QLDprint ='BRIS-PRINT01'
-$NTprint = 'DAR-PRINT01'
-$VICprint1 = 'MEL-PRINT01'
-$VICprint2 = 'MEL-PRINT02'
-$WAprint = 'PER-PRINT01'
-$TASprint = 'HOB-PRINT01'
-$SAprint = 'ADE-PRINT01'
+$Shares = @(
+    'CBR-PRINT01',  #ACT Print Server
+    'SYD-PRINT01',  #NSW Print Server
+    'BRIS-PRINT01', #QLD Print Server
+    'DAR-PRINT01',  #NT Print Server
+    'MEL-PRINT01',  #Vic Print Server 1
+    'MEL-PRINT02',  #Vic Print Server 2
+    'PER-PRINT01',  #WA Print Server
+    'HOB-PRINT01',  #Tas Print Server
+    'ADE-PRINT01',  #SA Print Server
+    'LON-PRINT01'   #UK Print Server
+)
 
 # Find installed Printers
-$GetPrint = Get-Printer | Format-List
-
-# Find office location
-if ($GetPrint -match $ACTprint) {
-    $PrintServ = $ACTprint
+foreach ($Share in $Shares) {
+    if ($Share -match $(Get-Printer | Format-List).ComputerName ) {
+        $NetPrinters = Get-Printer -ComputerName \\$Share | Format-table Name
+        foreach ($NetPrinter in $NetPrinters) {
+            Add-Printer -ConnectionName \\$PrintServer\$Printer
+        }
+        break
+    }
 }
-elseif ($GetPrint -match $NSWprint) {
-    $PrintServ = $NSWprint
-}
-elseif ($GetPrint -match $QLDprint) {
-    $PrintServ = $QLDprint
-}
-elseif ($GetPrint -match $NTprint) {
-    $PrintServ = $NTprint
-}
-elseif ($GetPrint -match $VICprint1) {
-    $PrintServ = $VICprint1
-}
-elseif ($GetPrint -match $VICprint2) {
-    $PrintServ = $VICprint2
-}
-elseif ($GetPrint -match $WAprint) {
-    $PrintServ = $WAprint
-}
-elseif ($GetPrint -match $TASprint) {
-    $PrintServ = $TASprint
-}
-elseif ($GetPrint -match $SAprint) {
-    $PrintServ = $SAprint
-}
-else {
-    EXIT 1
-}
-
-# Find all printers on print server
-$Printers = get-printer -ComputerName \\$PrintServ | Format-table name
-
-# Install each printer found
-foreach ($Printer in $Printers)
-Add-Printer -ConnectionName \\$PrintServ\$Printer
-
-# End Script

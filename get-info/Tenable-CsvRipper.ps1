4293a60219c740e8542929bd52a82490cea3807b
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-02 09:59:17 +1100

    Update Tenable-CsvRipper.ps1

--- get-info/Tenable-CsvRipper.ps1
+++ get-info/Tenable-CsvRipper.ps1
@@ -2,16 +2,18 @@
  .SYNOPSIS
     extracts the useful values from .csv files (dnsName, IP & file paths)
  .DESCRIPTION
-    extracts the following values from .csv files (specifically Tenable reports);
+    extracts values from .csv files (specifically Tenable reports);
      Machine dns name
      Machine IP address
+     (Otional filter) CVE number
     and matches them up with the file path of the affected file in the "pluginText" column.
 
     Written specifically to extract the file path of .jar files affected by CVE-2021-44228
-    from .csv output by Tenable. The file path/s were in a single value with a format like;
+    from .csv output by Tenable. 
+    CSV format is like;
     
         firstSeenDate,lastSeenDate,lastFixedDate,pluginID,pluginName,dnsName,ip,operatingSystem,cve,pluginText,solution    
-        21/04/2023,18/11/2024,,000000,Apache Log4j < 2.15.0 Remote Code Execution (Windows),server.domain,10.10.10.xx,Microsoft Windows Server 20xx Standard Build xxx,CVE-2021-44228,"<plugin_output>
+        21/04/2023,18/11/2024,,000000,Apache Log4j < 2.15.0 Remote Code Execution (Windows),server01.domain,10.10.10.xx,Microsoft Windows Server 20xx Standard Build xxx,CVE-2021-44228,"<plugin_output>
         Path              : C:\Program Files (x86)\xxx\xxx\lib\log4j-core-2.6.2.jar
         Installed version : 1
         Fixed version     : 2
@@ -23,7 +25,7 @@
         Fixed version     : 6
         </plugin_output>","Upgrade to Apache Log4j version 2.3.1 / 2.12.3 / 2.15.0 or later, or apply the vendor mitigation.
     
-    This script will pull this filepaths out & match them with the dnsName and IP address earlier in the file
+    This script will pull the filepaths out & match them with the dnsName and IP address earlier in the file
  .NOTES
     Author: Bill Wilson (https://github.com/Trael-Kun)
     Date:   29/11/2024
@@ -43,13 +45,13 @@ $Paths = @()
 $filePathRegex = "([a-zA-Z]:\\[^<>:""/\\|?*]+(?:\\[^<>:""/\\|?*]+)*)"
 #don't ask me how regex works, ask https://www.regular-expressions.info
 
-$Csv = Import-Csv -Path $InputCsv                                           #get the .csv data
+$Csv = Import-Csv -Path $InputCsv                                                               #get the .csv data
 
 foreach ($Row in $Csv) {
-    $Name   = $Row.DnsName                                                  #get the DNS Name
-    $IP     = $Row.Ip                                                       #get the IP
+    $Name   = $Row.DnsName                                                                      #get the DNS Name
+    $IP     = $Row.Ip                                                                           #get the IP
     $CveNo  = $Row.Cve
-    $Path   = [regex]::Matches($Row.PlugInText,$filePathRegex).value        #get the file paths
+    $Path   = [regex]::Matches($Row.PlugInText,$filePathRegex).value                            #get the file paths
     if ($Cve) {
         foreach ($File in $Path) {
             if ($Cve -eq $CveNo) {
@@ -59,9 +61,9 @@ foreach ($Row in $Csv) {
         }
     } else {
         foreach ($File in $Path) {                                              
-            $Info = [pscustomobject]@{DnsNameName=$Name; IP=$ip; Path=$File}    #organise data
-            $Paths += $Info                                                     #collate data
+            $Info = [pscustomobject]@{DnsNameName=$Name; IP=$ip; Path=$File}                    #organise data
+            $Paths += $Info                                                                     #collate data
         }
     }
 }
-Export-Csv -InputObject $Paths -Path $OutputCsv -Append -Force              #export data
+Export-Csv -InputObject $Paths -Path $OutputCsv -Append -Force                                  #export data

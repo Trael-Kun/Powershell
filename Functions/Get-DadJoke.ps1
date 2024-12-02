4fe662b72ade3b32d2d066f0105a3c9e731cedac
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-02 16:58:10 +1100

    Create Get-DadJoke.ps1

---
+++ Functions/Get-DadJoke.ps1
@@ -0,0 +1,14 @@
+function Get-DadJoke {
+     #Adapted from https://community.spiceworks.com/t/get-dadjoke/975466
+     Write-Output ''
+     $DadJoke = (Invoke-WebRequest -Uri "https://icanhazdadjoke.com" -Headers @{accept="application/json"} | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Joke).tostring()
+     if ($DadJoke -like "*`?*") {
+          $SplitJoke = $DadJoke.split('?')
+          Write-Host "$($SplitJoke[0])?" -ForegroundColor Magenta
+          Write-Host "$($SplitJoke[1].Trim())" -ForegroundColor Yellow
+          Write-Output ''
+     } else {
+          Write-Host $DadJoke -ForegroundColor Yellow
+          Write-Output ''
+     }
+}

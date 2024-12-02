bf7384722dc31501df60c5383aa17765fcbcd76e
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-02 15:52:05 +1100

    Create Write-Alphabet.ps1

---
+++ Functions/Write-Alphabet.ps1
@@ -0,0 +1,8 @@
+function Write-Alphabet {
+    [char]$Letter = 'A'
+    do {
+        write-host $Letter
+        start-sleep -Seconds 1
+        $Letter = [byte]$Letter + 1
+    } until ($Letter -gt 'z')
+}

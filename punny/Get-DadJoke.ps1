04c11b2228344395aecce57fff5ab5799af12b32
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-03 09:46:07 +1100

    Update Get-DadJoke.ps1

--- punny/Get-DadJoke.ps1
+++ punny/Get-DadJoke.ps1
@@ -2,7 +2,7 @@ function Get-DadJoke {
      #Adapted from https://community.spiceworks.com/t/get-dadjoke/975466
      Write-Output ''
      $DadJoke = (Invoke-WebRequest -Uri "https://icanhazdadjoke.com" -Headers @{accept="application/json"} | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Joke).tostring()
-     if ($DadJoke -like "*`?*") {
+     if ($DadJoke -contains '?') {
           $SplitJoke = $DadJoke.split('?')
           Write-Host "$($SplitJoke[0])?" -ForegroundColor Magenta
           Write-Host "$($SplitJoke[1].Trim())" -ForegroundColor Yellow

322b960f4c01e88ed52dc5bc0b3afc959e71a9ec
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-03 16:16:21 +1100

    Update Speak-CatFact.ps1
    added while loop

--- punny/Speak-CatFact.ps1
+++ punny/Speak-CatFact.ps1
@@ -1,11 +1,14 @@
 #https://www.reddit.com/r/PowerShell/comments/hsq84q/comment/fycz6ex
-Add-Type -AssemblyName System.Speech
-$SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$SpeechSynth.SelectVoice("Microsoft Zira Desktop")
-$Browser = New-Object System.Net.WebClient
-$Browser.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$CatFact = (ConvertFrom-Json (Invoke-WebRequest -Verbose -Uri https://catfact.ninja/fact -UseBasicParsing))
-$CatFact.fact
-$SpeechSynth.Speak("Did you know ?")
-$SpeechSynth.Speak($CatFact.fact)
+while ($true) {
+    Add-Type -AssemblyName System.Speech
+    $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $SpeechSynth.SelectVoice("Microsoft Zira Desktop")
+    $Browser = New-Object System.Net.WebClient
+    $Browser.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Verbose -Uri https://catfact.ninja/fact -UseBasicParsing))
+    $CatFact.fact
+    $SpeechSynth.Speak("Did you know ?")
+    $SpeechSynth.Speak($CatFact.fact)
+    Start-Sleep -Seconds 3600 
+}

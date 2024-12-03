f8ec37c2a3a6ff35db881d9a37feb0df1c32fa4e
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-03 12:12:08 +1100

    Update Get-DadJoke.ps1
    refined output formatting

--- punny/Get-DadJoke.ps1
+++ punny/Get-DadJoke.ps1
@@ -1,14 +1,20 @@
 function Get-DadJoke {
-     #Adapted from https://community.spiceworks.com/t/get-dadjoke/975466
-     Write-Output ''
-     $DadJoke = (Invoke-WebRequest -Uri "https://icanhazdadjoke.com" -Headers @{accept="application/json"} | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Joke).tostring()
-     if ($DadJoke -contains '?') {
-          $SplitJoke = $DadJoke.split('?')
-          Write-Host "$($SplitJoke[0])?" -ForegroundColor Magenta
-          Write-Host "$($SplitJoke[1].Trim())" -ForegroundColor Yellow
-          Write-Output ''
-     } else {
-          Write-Host $DadJoke -ForegroundColor Yellow
-          Write-Output ''
-     }
+    #Adapted from https://community.spiceworks.com/t/get-dadjoke/975466
+    Write-Output ''
+    $DadJoke = (Invoke-WebRequest -Uri "https://icanhazdadjoke.com" -Headers @{accept="application/json"} | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Joke).tostring()
+    if ($DadJoke -match "`n") {
+        #Anecdote
+        Write-Host $DadJoke -ForegroundColor Yellow
+        Write-Output ''
+    } elseif ($DadJoke -match "\?") {
+        #Question&Answer
+        $SplitJoke = $DadJoke.split("\?")
+        Write-Host "$($SplitJoke[0])?" -ForegroundColor Magenta
+        Write-Host "$($SplitJoke[1].Trim())" -ForegroundColor Yellow
+        Write-Output ''
+    } else {
+        #One-Liner
+        Write-Host $DadJoke -ForegroundColor Yellow
+        Write-Output ''
+    }
 }

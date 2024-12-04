aa706d05a9b439a9b875d60e456bedb3c27dc0d4
Bill <80299400+Trael-Kun@users.noreply.github.com>
2024-12-04 11:16:01 +1100

    Update Speak-CatFact.ps1
    added voice options

--- punny/Speak-CatFact.ps1
+++ punny/Speak-CatFact.ps1
@@ -1,14 +1,34 @@
 #https://www.reddit.com/r/PowerShell/comments/hsq84q/comment/fycz6ex
 while ($true) {
+    #set up voice
     Add-Type -AssemblyName System.Speech
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $SpeechSynth.SelectVoice("Microsoft Zira Desktop")
+    
+    #get voices
+    $Voices = $SpeechSynth.GetInstalledVoices().VoiceInfo
+    $PreferredVoice = 'Microsoft Catherine Desktop'
+    
+    #select Voice
+    if ($PreferredVoice -in $Voices.Name) {
+        $SpeechSynth.SelectVoice($PreferredVoice)
+    } elseif ($Voices.Count -eq 1) {
+            $SpeechSynth.SelectVoice($Voices.Name)
+    } elseif ('Microsoft Zira Desktop' -in $Voices.Name) {
+        $SpeechSynth.SelectVoice("Microsoft Zira Desktop")
+    } else {
+    }
+    
+    #set up to grab cat fact
     $Browser = New-Object System.Net.WebClient
     $Browser.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    
+    #get a fact
     $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Verbose -Uri https://catfact.ninja/fact -UseBasicParsing))
     $CatFact.fact
-    $SpeechSynth.Speak("Did you know ?")
+    
+    #say it
+    $SpeechSynth.Speak('Did you know ?')
     $SpeechSynth.Speak($CatFact.fact)
     Start-Sleep -Seconds 3600 
 }

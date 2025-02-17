b7391cb6fb94ea95990157ffe26bf76011da57cc
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-02-17 11:15:17 +1100

    Update PureEvil.ps1

--- punny/PureEvil.ps1
+++ punny/PureEvil.ps1
@@ -1,5 +1,5 @@
 function Clear-Clipboard {  #https://www.reddit.com/r/PowerShell/comments/1dlo6hx/comment/l9tqb6t
-    while ($True) {
+    while ($true) {
         if ($null -ne (Get-clipboard)) {
             Set-Clipboard $null
         }

ec9b57fc655c61367573fc5623e8d1e782793b40
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-02-18 09:51:49 +1100

    Update Do_or_donot.ps1

--- punny/Do_or_donot.ps1
+++ punny/Do_or_donot.ps1
@@ -1,5 +1,7 @@
-if ($xwing -in $swamp) {
-    DO {Move-Item $xwing -Destination $land -Force}
-    UNTIL ($swamp -notcontains $xwing)
+DO {
+    if ($xwing -in $swamp) {
+        DO {Move-Item $xwing -Destination $land -Force}
+        UNTIL ($swamp -notcontains $xwing)
+    }
 }
 #try{

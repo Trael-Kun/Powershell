169c590477226507cc317df5adfec3041559873d
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-02-26 11:50:17 +1100

    Update Semi-auto_patching.ps1
    added progress bars to pretty it up

--- Server/Semi-auto_patching.ps1
+++ Server/Semi-auto_patching.ps1
@@ -36,53 +36,65 @@ function Write-Count{
 }
 
 function Add-Cabs {
-    $CTemp = "C:\$Temp"
-    $Cabs = (Get-ChildItem -Path $CTemp -Recurse -Filter *.cab) | Sort-Object -Property LastWriteTime                           #find.cab files
-    
+    $CabStart   = Get-Date -Format "dd/mm/yy HH:MM:ss"
+    $CTemp      = "C:\$Temp"
+    $CabCount   = 0
+    $Cabs       = (Get-ChildItem -Path $CTemp -Recurse -Filter *.cab) | Sort-Object -Property LastWriteTime                     #find.cab files
+    Write-Count -ObjectDescription '.cab files' -Object $Cabs.Count -Positive
+    Write-Host ".Cab process started $CabStart" -ForegroundColor Cyan -BackgroundColor DarkCyan
     foreach ($Cab in $Cabs) {                                                                                                   #this should extract the KB number
-        foreach ($Kb in ($Cab.Name).Split('-')) {
-            if ($Kb -like "KB*") {
-                $CabCheck = $Kb
-                break                                                                                                           #we've got the KB, we don't need to keep looking
-            }
-        }
-        if (!((Get-HotFix -Id $CabCheck -ErrorAction SilentlyContinue) -or $0x800f081es -contains $CabCheck)) {                 #this checks if the KB has already been applied
+        Write-Progress -Activity Installing .cabs -Status $Cab.Name -PercentComplete (($CabCount/$Cabs.Count) * 100) -ParentId 1 -Id 2
+        $CabCheck = ($Cab.Split('-') | Select-String -Pattern kb*).ToString()                                                   #we've got the KB, we don't need to keep looking
+        $CabCount = $CabCount++
+        if (!((Get-HotFix -Id $CabCheck -ErrorAction SilentlyContinue) -or $Fails -contains $CabCheck)) {                       #this checks if the KB has already been applied
             $Space
-            Write-Host "Adding $CabCheck"
-            Write-Host "Started $(Get-Date -Format "dd/mm/yy HH:MM:ss")" -ForegroundColor DarkGreen -BackgroundColor Green      #sometimes these take a while, so it's good to log when they start
+            Write-Host "Adding $CabCheck ($CabCount of $($Cabs.Count))"
+            Write-Host "Started $(Get-Date -Format "dd/mm/yy HH:MM:ss")"     -ForegroundColor DarkGreen -BackgroundColor Green  #sometimes these take a while, so it's good to log when they start
             Add-WindowsPackage -PackagePath $Cab.FullName -Online -NoRestart -ErrorAction SilentlyContinue                      #add it
-            if (!($error[0].Exception -match $FailedMsg)) {                                                                     #if it flicks a 0x800f081e error, the .cab is for a different OS
-                $0x800f081es = $0x800f081es + $CabCheck
-                Write-Host $FailedMsg             -ForegroundColor Red  -BackgroundColor DarkRed
-                Write-Host "$Cabcheck"            -ForegroundColor Gray -BackgroundColor DarkRed -NoNewline
-                Write-Host ' not applicable'      -ForegroundColor Red  -BackgroundColor DarkRed
-                Write-Host 'CBS_E_NOT_APPLICABLE' -ForegroundColor Red
+            $Oops = $error[0].Exception
+            if (!($Oops -in $FailedMsgs)) {                                                                                     #if it flicks a 0x800f081e error, the .cab is for a different OS
+                $Fails = $Fails + ([Pscustomobject]@{Server=$Good; KB=$CabCheck; Error=$Oops})
+                Write-Host $error[0]                                         -ForegroundColor Red  -BackgroundColor DarkRed
+                Write-Host "$Cabcheck"                                        -ForegroundColor Gray -BackgroundColor DarkRed -NoNewline
+                Write-Host " not applicable ($CabCount of $($Cabs.Count))"    -ForegroundColor Red  -BackgroundColor DarkRed
             } else {
-                Write-Host "Finished $(Get-Date -Format "dd/mm/yy HH:MM:ss")" -ForegroundColor Green -BackgroundColor DarkGreen #mark when it's finished
+                Write-Host "Finished $(Get-Date -Format "dd/mm/yy HH:MM:ss")" -ForegroundColor DarkGreen -BackgroundColor Green #mark when it's finished
             }
         } else {
-            $Space
-            Write-Host "$CabCheck already present" -ForegroundColor Yellow -BackgroundColor DarkYellow                          #mark it as already installed
+            $Space                                                                                                              #mark it as already installed
+            Write-Host "$CabCheck already present ($CabCount of $($Cabs.Count))" -ForegroundColor Yellow -BackgroundColor DarkYellow
         }
     }
+    $Space
+    Write-Host "Removing local files"
     Remove-Item -Path $CTemp -Recurse -Force                                                                                    #delete the local .cab folder
+    $CabSpan = New-Timespan -Start $CabStart
+    $Space
+    Write-Host ".Cab process finished $(Get-Date -Format "dd/mm/yy HH:MM:ss")"  -ForegroundColor Cyan -BackgroundColor DarkCyan
+    Write-Host "Process on $Good took $CabSpan"                                 -ForegroundColor Cyan -BackgroundColor DarkCyan
 }
 
 #Variables
+$Start          = Get-Date -Format "dd/mm/yy HH:MM:ss"
 $Domain         = ".$(((Get-CimInstance -ClassName Win32_ComputerSystem).Domain).Split('.')[0])"                                #so you can remove the domain nmame from an CSV list from SCCM reporting
 $CabFiles       = Get-ChildItem -Path $SourceDir -Filter *.cab -Recurse                                                         #count how many files are on the source dir
 $Space          = Write-Output ''                                                                                               #makes to easier to see gaps in output
-$FailedMsg      = 'Add-WindowsPackage failed. Error Code = 0x800f081e'                                                          #error mesage for .cab installs for wrong OS
-$0x800f081es    = @()                                                                                                           #array for errored .cab installs
+$FailedMsgs     = 'Add-WindowsPackage failed. Error Code = 0x800f081e',                                                         #error mesage for .cab installs for wrong OS
+                  'Add-WindowsPackage failed. Error Code = 0x80070490'                                                          #error mesage for .cab installs for a component that was damaged or lost during an update
+$Fails          = @()                                                                                                           #array for errored .cab installs
 $Online         = @()                                                                                                           #array for online servers
 $Offline        = @()                                                                                                           #array for offline servers
+$CabCount       = 0
+$ServerCount    = 0
+
+Write-Host "Script started $Start" -ForegroundColor DarkCyan -BackgroundColor Cyan
 Write-Count -ObjectDescription 'servers' -Object $ServerList                                                                    #How many servers are in our list?
 $Space
 
 #Test Connection
 foreach ($Server in $ServerList) {
-    if ($Server -match "*$Domain") {                                                                                            #remove the domain name from servername, e.g. Server01-Test.Domain.Local
-        $Server = $Server -replace $Domain
+    if ($Server -match "*.$Domain") {                                                                                           #remove the domain name from servername, e.g. Server01-Test.Domain.Local
+        $Server = $Server -replace "*.$Domain*"
     }
     if (Test-Connection -ComputerName $Server -ErrorAction SilentlyContinue) {                                                  #is it online?
         $Online = $Online + $Server
@@ -92,7 +104,7 @@ foreach ($Server in $ServerList) {
 }                                                                                                                               #display results
 Write-Count -ObjectDescription 'servers offline' -Object $Offline -Report
 Write-Count -ObjectDescription 'servers online'  -Object $Online  -Report -Positive
-if ($Offline.Count -eq $Servers.Count -or $Online.Count -lt 1) {                                                                #if nmothing's online, we're done
+if ($Offline.Count -eq $Servers.Count -or $Online.Count -lt 1) {                                                                #if nothing's online, we're done
     $Space
     Write-Host 'No servers online' -ForegroundColor Red
     Write-Host 'Ending Script'
@@ -122,14 +134,16 @@ if ($DirBad.Count -eq $Servers.Count -or $DirGood.Count -lt 1) {
 #Copy Files 
 $Temp = 'Temp\Cab'
 foreach ($Good in $DirGood) {
+    $ServerCount++
+    Write-Progress -Activity 'Processing' -status $Good -PercentComplete (($ServerCount/$ServerList.count) * 100) -Id 1
     $DestDir = "\\$($Good)\c$\$Temp"
     $Space
     Write-Host "Copying $($CabFiles.count) files to $DestDir" -ForegroundColor Grey -BackgroundColor Black
     try { Copy-Item -Path $CabDir -Destination $DestDir -Recurse -Force                                                         #let's copy files
         try{                                                                                                                    #can we connect remotely?
             Write-Output "Connecting to $Good"
-            Enter-PSSession $Good
-            Add-Cabs
+            $G = New-PSSession -ComputerName $Good -ThrottleLimit 1
+            Invoke-Command -Session $G -Scriptblock {Add-Cabs}
             Exit-PSSession
         } catch {                                                                                                               #no? Can we send a remote command?
             Write-Host "Unable to connect to server $Good" -ForegroundColor Red
@@ -142,8 +156,10 @@ foreach ($Good in $DirGood) {
             }
         }
     } catch {
-        Write-Host "Unable to connect to $DestDir" -ForegroundColor Black -BackgroundColor Red
+        Write-Host "Unable to connect to $DestDir"      -ForegroundColor Black -BackgroundColor Red
         $DirBad = $DirBad + $Good
     }
 }
 $Space
+Write-Host "Script finished $(Get-Date -Format "dd/mm/yy HH:MM:ss")"                -ForegroundColor DarkCyan -BackgroundColor Cyan
+Write-Host "Script took $(New-TimeSpan -Start $Start)"  -ForegroundColor DarkCyan -BackgroundColor Cyan

055a860907678632a190e14e05ff5ffe4d5554cd
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-03-20 09:19:57 +1100

    Update Write-Log.ps1
    overcomplicated the whole thing

--- Functions/Write-Log.ps1
+++ Functions/Write-Log.ps1
@@ -1,23 +1,51 @@
 function Write-Log {
     param (
-        [Parameter(Mandatory=$true)]
-        [string]$Message,  #What you want the log to say
-        [Parameter(Mandatory=$false)]
-        [string]$LogFile,  #Can be set earlier in script by commenting out this parameter and declaring as a variable, e.g. $LogFile = 'C:\Temp\Log.txt'
-        [switch]$NoDate    #Can be set earlier in script by commenting out this parameter and declaring as a variable, e.g. $NoDate = $true
-    )
-    $DateTime   = [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
-    if ($LogFile) {
-        if ($NoDate) {
-            Write-Output $Message
-            Add-Content -Path $LogFile -Value "$Message" -Force
-        
+        [Parameter(Mandatory,HelpMessage='The message to be output to file & displayed onscreen')]
+        [string]$strMessage,
+        [Parameter(Mandatory=$false)] #these can be set earlier in script by commenting out this parameter and declaring as a variable, e.g. $LogFile = 'C:\Temp\Log.txt', $NoDate = $true, $UTC = $false, etc.
+        [switch]$NoLog,                                     #don't save a log
+        [switch]$NoDate,                                    #don't add the date
+        [switch]$UTC,                                       #time in UTC
+        [string]$LogFile = "$env:SystemDrive\Temp\Log.log", #where the log is stored
+        [ValidateSet(
+            'Information',
+            'Info',
+            'Warning',
+            'Warn',
+            'Error',
+            'Err',
+            $null
+        )]$MsgType
+    ) 
+
+    #set message type & colour
+    if ($MsgType -match 'Info') {
+        $Message = "Info:    $strMessage"
+        $Colour  = 'Green'
+    } elseif ($MsgType -match  'Warn') {
+        $Message = "Warning: $strMessage"
+        $Colour  = 'Yellow'
+    } elseif ($MsgType -like 'Err') {
+        $Message = "Error:   $strMessage"
+        $Colour  = 'Red'
+    } elseif ($null -eq $MsgType) {
+        $Message = $strMessage
+        $Colour  = 'White'
+    }
+
+    if (!($NoDate)) {
+        if ($UTC) {
+            $DateTime   = [DateTime]::UtcNow.ToString('yyyy-MM-dd HH:mm:ss UTC')
         } else {
-            Write-Output $Message
-            Add-Content -Path $LogFile -Value "$DateTime	|	$Message"  -Force
+            $DateTime   = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
         }
+        $Message        = "$DateTime    | $Message" 
+    }
+
+    if ($NoLog) {
+        Write-Host $Message -ForegroundColor $Colour
     } else {
-        Write-Host "Log File Not Set" -ForegroundColor Red -BackgroundColor Black
-        Write-Output "$DateTime	|	$Message"
+        Write-Host $Message -ForegroundColor $Colour
+        Add-Content -Path $LogFile -Value "$Message" -Force
     }
 }

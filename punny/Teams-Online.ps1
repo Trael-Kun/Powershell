6e5e13de1eff550abd7646f7fe7a0581a46aebb4
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-03-21 12:53:25 +1100

    Update Teams-Online.ps1
    added date & time variables

--- punny/Teams-Online.ps1
+++ punny/Teams-Online.ps1
@@ -1,15 +1,44 @@
-#https://powershellisfun.com/2024/05/23/keep-microsoft-teams-status-available-instead-of-away-using-powershell/
+#Adapted from; https://powershellisfun.com/2024/05/23/keep-microsoft-teams-status-available-instead-of-away-using-powershell/
+
+param(
+    [Parameter(Mandatory)]
+    $Out     = '12:30',
+    $In      = '13:00',
+    $Days    = ('Monday','Friday'),
+    [int32]$Sec = 60,
+    [Parameter(Mandatory=$false)]
+    [string]$App
+)
+
+function Send-Numlock {
+    $wshell = New-Object -ComObject wscript.shell
+    $wshell.sendkeys("{NUMLOCK}{NUMLOCK}")
+    Write-Host ("{0}	- Pressed NUMLOCK twice and waiting for $Sec seconds" -f $(get-date)) -ForegroundColor Green
+    Start-Sleep -Seconds $Sec
+}
+
+$TimeOut = (Get-Date -DisplayHint Time -Format $Out)
+$TimeIn  = (Get-Date -DisplayHint Time -Format $In)
+
 while ($true) {
-    try {
-        Get-Process -Name 'ms-teams' -ErrorAction stop | Out-Null
-        Write-Host ("{0}	- Microsoft Teams is running..." -f $(get-date)) -ForegroundColor Green
-        $wshell = New-Object -ComObject wscript.shell
-        $wshell.sendkeys("{NUMLOCK}{NUMLOCK}")
-        Write-Host ("{0}	- Pressed NUMLOCK twice and waiting for 60 seconds" -f $(get-date)) -ForegroundColor Green
-        Start-Sleep -Seconds 60          
-    }
-    catch {
-        Write-Warning ("{0}	- Microsoft Teams is not running, sleeping for 15 seconds..." -f $(get-date))
-        Start-Sleep -Seconds 15
+    $Day = Get-Date -Format dddd
+    if ($Day -in $Days) {
+        if ($(Get-Date) -lt [datetime]$(Get-Date -Format $TimeOut) -or $(Get-Date) -gt [datetime]$(Get-Date -Format $TimeIn)) {
+            if ($App) {
+                try {
+                    Get-Process -Name $App -ErrorAction stop | Out-Null
+                    Write-Host ("{0}	- $App is running..." -f $(get-date)) -ForegroundColor Green
+                    Send-Numlock
+                } catch {
+                    Write-Warning ("{0}	- $App is not running, sleeping for $($Sec / 2) seconds..." -f $(get-date))
+                    Start-Sleep -Seconds ($Sec / 2)
+                }
+            } else {        
+                Send-Numlock
+            }
+        } else {
+            Write-Host ("{0}	- Waiting for $($Sec * 2) seconds" -f $(get-date)) -ForegroundColor DarkGreen
+            Start-Sleep -Seconds ($Sec * 2)
+        }
     }
 }

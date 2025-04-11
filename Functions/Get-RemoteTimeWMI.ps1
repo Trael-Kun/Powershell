0c1a061e25f17e167fa82441bc9ecedad76c758a
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-04-11 17:14:37 +1000

    Create Get-RemoteTimeWMI.ps1

---
+++ Functions/Get-RemoteTimeWMI.ps1
@@ -0,0 +1,26 @@
+#https://learn.microsoft.com/en-us/windows/win32/wmisdk/wmi-tasks--dates-and-times
+function Get-RemoteTimeWMI{
+    param (
+        [string]$Computer = $env:COMPUTERNAME,
+        [switch]$Show,
+        [switch]$Fancy
+    )
+    #get Remote Time
+    $Times       = Get-WmiObject Win32_LocalTime -Computer $Computer
+    #Organise Data
+    $PcName      = $Times.PSComputerName
+    $Second      = $Times.Second
+    $Minute      = $Times.Minute
+    $Hour        = $Times.Hour
+    $Day         = $Times.Day
+    $Month       = $Times.Month
+    $Year        = $Times.Year
+    $DayOfWeek   = (Get-Date $Day/$Month/$Year).DayOfWeek
+    $MonthOfYear = Get-Date -Month $Month -Format MMMM
+    <# Now display the result #>
+    if ($Show -and $Fancy) {
+        Write-Output "Current time for $PcName; $($Hour):$Minute $DayOfWeek $Day of $MonthOfYear $Year"
+    } elseif ($Show) {
+        Write-Output "Current time for $PcName; $($Hour):$Minute, $Day/$Month/$Year"
+    }
+}

2b0b97919146d0b12cc6e5da3c469c18474132f2
Bill <80299400+Trael-Kun@users.noreply.github.com>
2025-05-14 17:14:12 +1000

    Update XmasLights.ps1

--- punny/XmasLights.ps1
+++ punny/XmasLights.ps1
@@ -2,17 +2,48 @@
 Bill Wilson (https://github.com/Trael-Kun)
 13/05/25
 #>
-
-function Get-Light {
-if ($Bott % 2 -eq 0) {
-    $Col = 'Red'
-    $Light = '·'
-  } else {
-    $Col = 'Green'
-    $Light = '.'
-  }
+function Clear-HostLight {
+    #https://www.reddit.com/r/PowerShell/comments/a3pval/comment/ebbditq
+    Param (
+        [Parameter(Position=1)]
+        [int32]$Count=1
+    )
+    $CurrentLine  = $Host.UI.RawUI.CursorPosition.Y
+    $ConsoleWidth = $Host.UI.RawUI.BufferSize.Width
+    $i = 1
+    for ($i; $i -le $Count; $i++) {
+        [Console]::SetCursorPosition(0,($CurrentLine - $i))
+        [Console]::Write("{0,-$ConsoleWidth}" -f " ")
+    }
+    [Console]::SetCursorPosition(0,($CurrentLine - $Count))
 }
-#Start
+$a = $true
+while ($true) {
+    $Loop = 0
+    if ($a) {
+        $a = $false
+    } else { $a = $true}
+    while ($Loop -lt 10) {
+        if ($a) {
+            $Color1 = 'Red'
+            $Light1 = '·'
+            $Color2 = 'Green'
+            $Light2 = '.'
+        } else {
+            $Color1 = 'Green'
+            $Light1 = '.'
+            $Color2 = 'Red'
+            $Light2 = '·'
+        }
+        $Loop++
+        Write-Host "$Light1 " -NoNewLine -ForegroundColor $Color1 
+        Write-Host "$Light2 " -NoNewLine -ForegroundColor $Color2
+    }
+    Write-host ''
+    Start-Sleep -Seconds 1
+    Clear-HostLight -Count 1
+}
+<#Start
 $Size = 10
 $Top = 0
 while ($Top -lt $Size) {
@@ -37,3 +68,4 @@ while ($Bott -lt $Size) {
 }
 Write-Host ''
 #End
+#>

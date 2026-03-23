5baa72ad7e3e8cf8a975953300b452631bfee81f
Bill <80299400+Trael-Kun@users.noreply.github.com>
2026-03-24 10:53:19 +1100

    Update Get-RoundedHour.ps1

--- Functions/Get-RoundedHour.ps1
+++ Functions/Get-RoundedHour.ps1
@@ -1,9 +1,45 @@
 function Get-RoundedHour {
-    param([datetime]$Date = (Get-Date))
-    $RoundedDate = if ($Date.Minute -ge 30) {   #Round Up: add an hour & set min/sec/millisec to 0
-        $Date.AddHours(1).AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
-    } else {                                    #Round Dn: set min/sec/millisec to 0
-        $Date.AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
+        <#
+            .DESCRIPTION
+             Round time up or down
+            .NOTES
+             Written by Bill 24/03/26
+        #>
+    param(
+        [datetime]$Date = (Get-Date),
+        [switch]$Up,
+        [Alias('Down,Dow')]
+        [switch]$Dn,
+        [ValidateSet(
+            'Hour',
+            'Hr',
+            'Minute',
+            'Min',
+            'Second',
+            'Sec',
+            'Millisecond',
+            'Mil')]
+        [string]$RoundTo = "Hr"
+    )
+
+    $RoundedDate = if ($Date.Minute -ge 30 -or $Up) {   #Round Up: add an hour & set min/sec/millisec to 0
+        if ($RoundTo -match "H*") {
+            $Date.AddHours(1).AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
+        } elseif ($RoundTo -Match "Min*") {
+            $Date.AddMinutes(1).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
+        } elseif ($RoundTo -Match "S*") {
+            $Date.AddSeconds(1).AddMilliseconds(-$Date.Millisecond)
+        } elseif ($RoundTo -match "Mil*") {
+            $Date.AddMilliseconds(1)
+        }
+    } elseif ($Date.Minute -lt 30 -or $Dn) {            #Round Dn: set min/sec/millisec to 0
+        if ($RoundTo -match "H*") {
+            $Date.AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
+        } elseif ($RoundTo -Match "Min*") {
+            $Date.AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
+        } elseif ($RoundTo -Match "S*") {
+            $Date.AddMilliseconds(-$Date.Millisecond)
+        }
     }
     return $RoundedDate
 }

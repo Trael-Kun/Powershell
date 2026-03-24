2e6129536fae5a5679d136d969f533407c81e5a9
Bill <80299400+Trael-Kun@users.noreply.github.com>
2026-03-24 14:53:28 +1100

    Update Get-RoundedHour.ps1

--- Functions/Get-RoundedHour.ps1
+++ Functions/Get-RoundedHour.ps1
@@ -1,22 +1,54 @@
 function Get-RoundedHour {
     <#
+    .SYNOPSIS
+    Round time up or down
+    
     .DESCRIPTION
-        Round time up or down
+    Rounds the entered time to the nearest unit - defaults to Hour
+    Automatically rounds time up or down, or accepts parameters to force direction
+
+    .PARAMETER Date
+    Allows entering date/time. Defaults to Now
+
+    .PARAMETER Up
+    Forces rounding up
+    
+    .PARAMETER Dn
+    Forces rounding down
+
+    .PARAMETER RoundTo
+    Sets unit to round to, accepts 'Hour', 'Minute', 'Second'
+    Defaults to hour
+
+    .PARAMETER Format
+    Optional output formatting
+
     .EXAMPLE
-        Get-RoundedHour -Up
+    PS> Get-RoundedHour -Up
+    Tuesday, 24 March 2026 3:00:00 PM
+
     .EXAMPLE
-        Get-RoundedHour -Down
+    PS> Get-RoundedHour -Down
+    Tuesday, 24 March 2026 2:00:00 PM
+
     .EXAMPLE
-        Get-RoundedHour -RoundTo Min
+    PS> Get-RoundedHour -RoundTo Min
+    Tuesday, 24 March 2026 2:52:00 PM
+
     .EXAMPLE
-        Get-RoundedHour -Dn -RoundTo Day -Format m
+    PS> Get-RoundedHour -Dn -RoundTo Sec -Format m
+    02:52
+
+    .LINK
+    https://github.com/Trael-Kun/Powershell/blob/main/Functions/Get-RoundedHour.ps1
+
     .NOTES
         Written by Bill 24/03/26
     #>
     param(
         [datetime]$Date = (Get-Date),
         [switch]$Up,
-        [Alias('Down,Dow')]
+        [Alias('Down')]
         [switch]$Dn,
         [ValidateSet(
             'Hour',         #Hour
@@ -26,7 +58,7 @@ function Get-RoundedHour {
             'Second',       #Second
             'Sec'           #Second
             )]
-        [string]$RoundTo = "Hr",
+        [string]$RoundTo = "Hour",
         [ValidateSet(
             'Y',    #Year
             'y',    #Year
@@ -45,21 +77,21 @@ function Get-RoundedHour {
     )
 
     function Set-RoundingUp {
-        if ($RoundTo -match "H*") {             #Round Up: add an hour & set min/sec/millisec to 0
+        if ($To -match "H*") {             #Round Up: add an hour & set min/sec/millisec to 0
             $Date.AddHours(1).AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
-        } elseif ($RoundTo -Match "Min*") {     #Round Up: add a minute & set sec/millisec to 0
+        } elseif ($To -Match "Min*") {     #Round Up: add a minute & set sec/millisec to 0
             $Date.AddMinutes(1).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
-        } elseif ($RoundTo -Match "S*") {       #Round Up: add a second & set millisec to 0
+        } elseif ($To -Match "S*") {       #Round Up: add a second & set millisec to 0
             $Date.AddSeconds(1).AddMilliseconds(-$Date.Millisecond)
         }
     }
 
     function Set-RoundingDn {
-        if ($RoundTo -match "H*") {                                     #Round Dn: set min/sec/millisec to 0
+        if ($To -match "H*") {                                     #Round Dn: set min/sec/millisec to 0
             $Date.AddMinutes(-$Date.Minute).AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
-        } elseif ($RoundTo -Match "Min*") {                             #Round Dn: set sec/millisec to 0
+        } elseif ($To -Match "Min*") {                             #Round Dn: set sec/millisec to 0
             $Date.AddSeconds(-$Date.Second).AddMilliseconds(-$Date.Millisecond)
-        } elseif ($RoundTo -Match "S*" -or $RoundTo -match "Mil*") {    #Round Dn: set millisec to 0
+        } elseif ($To -Match "S*" -or $To -match "Mil*") {    #Round Dn: set millisec to 0
             $Date.AddMilliseconds(-$Date.Millisecond)
         }
     }
@@ -69,7 +101,7 @@ function Get-RoundedHour {
     if ($RoundTo -match "H*") {
         $Rounding = $Date.Minute
         $Unit     = 30
-    } elseif ($RoundTo -Match "Min*") {
+    } elseif ($RoundTo -match "Min*") {
         $Rounding = $Date.Second
         $Unit     = 30
     } elseif ($RoundTo -Match "S*") {
@@ -78,13 +110,13 @@ function Get-RoundedHour {
     }
 
     $RoundedDate = if ($Up) {
-        Set-RoundingUp
+        Set-RoundingUp -To $RoundTo
     } elseif ($Dn) {
-        Set-RoundingDn
+        Set-RoundingDn -To $RoundTo
     } elseif ($Rounding -ge $Unit) {
-        Set-RoundingUp
+        Set-RoundingUp -To $RoundTo
     } elseif ($Rounding -lt $Unit) {
-        Set-RoundingDn
+        Set-RoundingDn -To $RoundTo
     }
 
     #Format
@@ -106,5 +138,29 @@ function Get-RoundedHour {
         $RoundedDate = "{0:hh:mm:ss:fff}" -f $RoundedDate
     }
 
+    #Write the result
     return $RoundedDate
 }
+
+<# Example usage
+$CurrentTime = Get-Date
+$RoundedTime = Get-RoundedHour -Date $(Get-Date) -Up
+#>
+
+Write-Host "Current Time: $(Get-Date)"
+Write-Host "Rounded Time: $RoundedTime"
+
+while ($True) {
+    if ($RoundedTime -lt $CurrentTime) {
+        "Current time is $(Get-Date -Format HH:mm), Rounded is $RoundedTime"
+        "Hour has passed"
+        #Write-Host "$Complete packages done by $RoundedTime"
+        $RoundedTime = Get-RoundedHour -Date $(Get-Date) -Up
+    } else {
+        "Current time is $(Get-Date -Format HH:mm), Rounded is $RoundedTime"
+        "Hour has not passed"
+        #dun do nuffin
+    }
+    Start-Sleep -Seconds 30
+}
+
